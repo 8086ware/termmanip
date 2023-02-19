aa38fa4b1f0e78a930e7d41357ce3cc69ec61030
8086ware <kirillos.khan08@outlook.com>
2023-02-18 19:35:36 -0500

    Add window/print.c. Provides printing to window functions. check_wrap_line() checks if the cursor is beyond the window columns, and wraps the window cursor to the next line.

---
+++ src/window/print.c
@@ -0,0 +1,41 @@
+#include "termmanip.h"
+#include "append_win.h"
+#include <string.h>
+
+int check_wrap_line(Tm_window* win) {
+	int ret = 0;
+
+	if(win->cursor_x > win->columns) {
+		if((ret = tm_win_cursor(win, 0, win->cursor_y + 1))) {
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+int tm_win_print_str(Tm_window* win, char* text) {
+	int ret = 0;
+
+	for(; *text!= '\0'; text++) {
+		if((ret = tm_win_print_ch(win, *text))) {
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+int tm_win_print_ch(Tm_window* win, char ch) {
+	int ret = 0;
+	
+	append_win(win, &ch);
+
+	win->cursor_x++;
+
+	if((ret = check_wrap_line(win))) {
+			return ret;
+	}
+
+	return 0;
+}
