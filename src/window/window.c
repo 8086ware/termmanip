657f310c3a59f336ee7a15410c40c31d88bd7bdb
8086ware <kirillos.khan08@outlook.com>
2023-02-18 19:16:05 -0500

    Put tm_get_winsize and tm_get_winpos in different folders, add tm_win_free function, make sure cursor is set to the window 0, 0, and put parantheses around tm_win_modify if statement to prevent warning

--- src/window/window.c
+++ src/window/window.c
@@ -1,15 +1,17 @@
 #include "termmanip.h"
+#include <stdlib.h>
+#include "exit_log.h"
 
 Tm_window* tm_window(int x, int y, int columns, int rows) {
-	int ret = 0;
-	
 	Tm_window* win = malloc(sizeof(Tm_window));
 
 	if(win == NULL) {
 		exit_log("tm_window", "malloc", 1);
 	}
+
+	int ret = 0;
 	
-	if(ret = tm_win_modify(win, x, y, columns, rows)) {
+	if((ret = tm_win_modify(win, x, y, columns, rows))) {
 		return (Tm_window*)ret;
 	}
 
@@ -21,15 +23,12 @@ Tm_window* tm_window(int x, int y, int columns, int rows) {
 	win->contents = NULL;
 	win->content_len = 0;
 
+	tm_win_cursor(win, 0, 0);
+
 	return win;
 }
 
-void tm_get_winsize(struct Window* win, int* columns, int* rows) {
-	*columns = win->columns;
-	*rows = win->rows;
-}
-
-void tm_get_winpos(struct Window* win, int* position_x, int* position_y) {
-	*position_x = win->position_x;
-	*position_y = win->position_y;
+void tm_win_free(Tm_window* win) {
+	free(win->contents);
+	free(win);
 }
