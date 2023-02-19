396144b98b5e625acf9ae042b526212f6f73a305
8086ware <kirillos.khan08@outlook.com>
2023-02-19 04:14:26 -0500

    Update test.c add new function tests

--- test/test.c
+++ test/test.c
@@ -3,11 +3,16 @@
 int main(void) {
 	tm_init();
 
-	Tm_window* test = tm_window(1, 1, 5, 5);
+	Tm_window* test = tm_window(5, 5, 20, 10);
+	Tm_window* test_child = tm_window(5, 5, 3, 3);
 	
+	tm_win_parent(test, test_child);
+
 	tm_win_border(test);
-	
+	tm_win_border(test_child);
+
 	tm_win_update(test);
+
 	getchar();
 
 	tm_exit();
