1dcad21bc48b9a361133ccaf3d5f222fc203e5de
8086ware <kirillos.khan08@outlook.com>
2023-02-21 23:20:04 -0500

    update test

--- test/test.c
+++ test/test.c
@@ -4,13 +4,10 @@ int main(void) {
 	tm_init();
 
 	Tm_window* test = tm_window(5, 5, 20, 10);
-	
-	tm_win_parent(default_win, test);
 
-	tm_win_border(default_win);
-	tm_win_border(test);
+	tm_win_print_str(test, "Ok buddy retard");
 
-	tm_update();
+	tm_win_update(test);
 
 	getchar();
 
