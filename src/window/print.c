43c1643cb1721baffce1ac1af1f0c77f236d9eb9
8086ware <kirillos.khan08@outlook.com>
2023-02-21 23:20:22 -0500

    Fix line wrap and stop from writing when cursor y exceeds window rows

--- src/window/print.c
+++ src/window/print.c
@@ -2,10 +2,18 @@
 #include "append_win.h"
 #include <string.h>
 
+int check_cursor_over_rows(Tm_window* win) {
+	if(win->cursor_y >= win->rows) {
+		return TM_INVALID_CURSOR;
+	}
+
+	return 0;
+}
+
 int check_wrap_line(Tm_window* win) {
 	int ret = 0;
 
-	if(win->cursor_x > win->columns - 1) {
+	if(win->cursor_x >= win->columns) {
 		if((ret = tm_win_cursor(win, 0, win->cursor_y + 1))) {
 			return ret;
 		}
@@ -16,6 +24,10 @@ int check_wrap_line(Tm_window* win) {
 
 int tm_win_print_str(Tm_window* win, char* text) {
 	int ret = 0;
+
+	if((ret = check_cursor_over_rows(win) == TM_INVALID_CURSOR)) {
+		return ret;
+	}
 
 	for(; *text!= '\0'; text++) {
 		if((ret = tm_win_print_ch(win, *text))) {
@@ -28,7 +40,11 @@ int tm_win_print_str(Tm_window* win, char* text) {
 
 int tm_win_print_ch(Tm_window* win, char ch) {
 	int ret = 0;
-	
+
+	if((ret = check_cursor_over_rows(win) == TM_INVALID_CURSOR)) {
+		return ret;
+	}
+
 	append_win(win, &ch);
 
 	win->cursor_x++;
