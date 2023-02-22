ff43f8ab270623bca75bbee83021607a01670357
8086ware <kirillos.khan08@outook.com>
2023-02-21 22:29:38 -0500

    Update test

--- test/test.c
+++ test/test.c
@@ -4,14 +4,13 @@ int main(void) {
 	tm_init();
 
 	Tm_window* test = tm_window(5, 5, 20, 10);
-	Tm_window* test_child = tm_window(5, 5, 3, 3);
 	
-	tm_win_parent(test, test_child);
+	tm_win_parent(default_win, test);
 
+	tm_win_border(default_win);
 	tm_win_border(test);
-	tm_win_border(test_child);
 
-	tm_win_update(test);
+	tm_update();
 
 	getchar();
 
