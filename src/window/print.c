583aa6dc33a2d8582f20d3807689fef174091eaf
8086ware <kirillos.khan08@outlook.com>
2023-02-23 15:34:17 -0500

    Fix line wrap, and check cursor position BEFORE writing to the window, to prevent writing outside of window after exceeding the row number. Also remove check over rows, because the tm_win_cursor already does that for us.

--- src/window/print.c
+++ src/window/print.c
@@ -2,14 +2,6 @@
 #include "append_win.h"
 #include <string.h>
 
-int check_cursor_over_rows(Tm_window* win) {
-	if(win->cursor_y >= win->rows) {
-		return TM_INVALID_CURSOR;
-	}
-
-	return 0;
-}
-
 int check_wrap_line(Tm_window* win) {
 	int ret = 0;
 
@@ -25,10 +17,6 @@ int check_wrap_line(Tm_window* win) {
 int tm_win_print_str(Tm_window* win, char* text) {
 	int ret = 0;
 
-	if((ret = check_cursor_over_rows(win) == TM_INVALID_CURSOR)) {
-		return ret;
-	}
-
 	for(; *text!= '\0'; text++) {
 		if((ret = tm_win_print_ch(win, *text))) {
 			return ret;
@@ -41,17 +29,13 @@ int tm_win_print_str(Tm_window* win, char* text) {
 int tm_win_print_ch(Tm_window* win, char ch) {
 	int ret = 0;
 
-	if((ret = check_cursor_over_rows(win) == TM_INVALID_CURSOR)) {
-		return ret;
+	if((ret = check_wrap_line(win))) {
+			return ret;
 	}
 
 	append_win(win, &ch);
 
 	win->cursor_x++;
-
-	if((ret = check_wrap_line(win))) {
-			return ret;
-	}
 
 	return 0;
 }
