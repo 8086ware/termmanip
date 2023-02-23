6fdb07481b2444a59c7f079b4f7b04c4d7812baf
8086ware <kirillos.khan08@outlook.com>
2023-02-23 15:37:46 -0500

    update test

--- test/test.c
+++ test/test.c
@@ -1,15 +1,27 @@
 #include "termmanip.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 int main(void) {
 	tm_init();
 
-	Tm_window* test = tm_window(5, 5, 20, 10);
+	tm_win_border(default_win);
 
-	tm_win_print_str(test, "Ok buddy retard");
+	int i = 0;
+	int disp = 0;
+	char k[32];
 
-	tm_win_update(test);
+	while(1) {
+		i++;
+		if(clock() % CLOCKS_PER_SEC == 0) {
+			disp = i;
+			i = 0;
+			sprintf(k, "%d, seconds: %f",  disp, (float)clock() / CLOCKS_PER_SEC);	
+			tm_cursor(1, 1);
+			tm_print_str(k);
+			tm_update();
+		}
+	}
 
-	getchar();
-
-	tm_exit();
 }
