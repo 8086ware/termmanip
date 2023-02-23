f2251b41d45a3b966a0086acc1f21e5f7a653e26
8086ware <kirillos.khan08@outlook.com>
2023-02-23 17:51:25 -0500

    Update window, then update children

--- src/window/update.c
+++ src/window/update.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 
 void tm_win_update(Tm_window* win) {
+	write(fileno(stdout), win->contents, win->content_len);
+
 	if(win->children != NULL) {
 		for(int i = 0; i < win->children_amount; i++) {
 			tm_win_update(win->children[i]);
 		}
 	}
 
-	write(fileno(stdout), win->contents, win->content_len);
-	
+
 	tm_win_clear(win);
 }
