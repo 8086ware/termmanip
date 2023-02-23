061218be0b2a1f7a7aa51fec680ec23069b88056
8086ware <kirillos.khan08@outlook.com>
2023-02-23 16:52:50 -0500

    Handle ansi characters

--- src/window/print.c
+++ src/window/print.c
@@ -2,6 +2,12 @@
 #include "append_win.h"
 #include <string.h>
 
+void handle_escape_codes(Tm_window* win, char escape) {
+	if(escape == '\n') {
+		tm_win_cursor(win, 0, win->cursor_y + 1);
+	}
+}
+
 int check_wrap_line(Tm_window* win) {
 	int ret = 0;
 
@@ -31,6 +37,11 @@ int tm_win_print_ch(Tm_window* win, char ch) {
 
 	if((ret = check_wrap_line(win))) {
 			return ret;
+	}
+	
+	if(ch == '\n') {
+		handle_escape_codes(win, ch);
+		return 0;
 	}
 
 	append_win(win, &ch);
