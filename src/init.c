769a9ce527ed760fccd0f20211a0aee3c2119ec2
8086ware <kirillos.khan08@outlook.com>
2023-02-24 02:55:18 -0500

    Go back to create default_win with tm_window

--- src/init.c
+++ src/init.c
@@ -25,28 +25,12 @@ void tm_init() {
 	int scr_columns, scr_rows;
 
 	tm_get_scrsize(&scr_columns, &scr_rows);
+	default_win = tm_window(0, 0, scr_columns, scr_rows);
 
-	default_win = malloc(sizeof(Tm_window));
-
-	if(default_win == NULL) {
-		exit_log("tm_window", "malloc", 1);
-	}
-
-	tm_win_modify(default_win, 0, 0, scr_columns, scr_rows);
-
-	default_win->parent = NULL;
-	default_win->children = NULL;
-
-	default_win->children_amount = 0;
-
-	default_win->contents = NULL;
-	default_win->content_len = 0;
-	
-	append_win(default_win, TM_ESC_ENTER_ALT_SCREEN);
-	append_win(default_win, "\x1b[2J");
-
-	tm_win_cursor(default_win, 0, 0);
-	tm_update();
+	write(fileno(stdout), TM_ESC_ENTER_ALT_SCREEN, strlen(TM_ESC_ENTER_ALT_SCREEN));
+	write(fileno(stdout), "\x1b[2J", strlen("\x1b[2J"));
+		
 	tm_echo(0);
+	tm_rawinput(1);
 }
 
