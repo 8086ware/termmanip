831ee3ea31739cc014b525bc25aef33003109ea0
8086ware <kirillos.khan08@outlook.com>
2023-02-23 21:41:21 -0500

    Add windows support

--- src/init.c
+++ src/init.c
@@ -1,11 +1,25 @@
 #include "termmanip.h"
 #include <stdio.h>
 #include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+#include <io.h>
+#else
 #include <unistd.h>
+#endif
 
 void tm_init() {
-	int scr_columns, scr_rows;
+#ifdef _WIN32
+	DWORD console_mode = 0;
 
+	GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &console_mode);
+
+	console_mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	
+	SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), console_mode);
+
+	int scr_columns, scr_rows;
+#endif
 	tm_get_scrsize(&scr_columns, &scr_rows);
 	default_win = tm_window(0, 0, scr_columns, scr_rows);
 	write(fileno(stdout), TM_ESC_ENTER_ALT_SCREEN, strlen(TM_ESC_ENTER_ALT_SCREEN));
--- src/screen_size.c
+++ src/screen_size.c
@@ -1,20 +1,43 @@
 #include "termmanip.h"
+#ifndef _WIN32
 #include <sys/ioctl.h>
+#else
+#include <windows.h>
+#endif
 
 void tm_get_scrsize(int* x, int* y) {
+#ifndef _WIN32
 	struct winsize ws;
 
 	ioctl(1, TIOCGWINSZ, &ws);
 
 	*x = ws.ws_col;
 	*y = ws.ws_row;
+#else
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+	*x = csbi.dwSize.X;
+	*y = csbi.dwSize.Y;
+#endif
 }
 
 void tm_set_scrsize(int x, int y) {
+#ifndef _WIN32
 	struct winsize ws;
 
 	ws.ws_col = x;
 	ws.ws_row = y;
 
 	ioctl(1, TIOCSWINSZ, &ws);
+#else
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+	csbi.dwSize.X = x;
+	csbi.dwSize.Y = y;
+
+	COORD coord = {x,y};
+	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+#endif
 }
