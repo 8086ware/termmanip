a9ecf6b554c627e10347c0c2f49dd3c7d22d965e
8086ware <kirillos.khan08@outlook.com>
2023-02-25 19:27:29 -0500

    Added size parameter to append_win_str and changed len vvariable to size. This is more safer then determining the size without null terminator incase there is none.

--- src/window/append_win.c
+++ src/window/append_win.c
@@ -4,33 +4,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-void append_win_str(Tm_window* win, char* data) {
-	int len = strlen(data);
-	win->contents = realloc(win->contents, win->content_len + len + 1);
+void append_win_str(Tm_window* win, char* data, int size) {
+	win->contents = realloc(win->contents, win->content_len + size);
 
 	if(win->contents == NULL) {
 		exit_log("append_win", "realloc", 1);
 	}
 
-	for(int i = 0; i < len; i++) {
+	for(int i = 0; i < size; i++) {
 		win->contents[win->content_len + i] = data[i];
 	}
 
-	win->content_len += len;
+	win->content_len += size;
 }
 
 void append_win_ch(Tm_window* win, char data) {
-	int len = 1;
-	win->contents = realloc(win->contents, win->content_len + len + 1);
+	int size = 1;
+	win->contents = realloc(win->contents, win->content_len + size);
 
 	if(win->contents == NULL) {
 		exit_log("append_win", "realloc", 1);
 	}
 
-	for(int i = 0; i < len; i++) {
+	for(int i = 0; i < size; i++) {
 		win->contents[win->content_len + i] = data;
 	}
 
-	win->content_len += len;
-
+	win->content_len += size;
 }
