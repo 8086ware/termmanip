1a808ee73fd7b2e362fb5265210d060fc3b4d335
8086ware <kirillos.khan08@outlook.com>
2023-02-26 04:09:28 -0500

    Remove unnecessary "+ 1"

--- src/window/update.c
+++ src/window/update.c
@@ -9,7 +9,7 @@ void tm_win_update(Tm_window* win) {
 		}
 	}
 
-	write(fileno(stdout), win->contents, win->content_len +  1);
+	write(fileno(stdout), win->contents, win->content_len);
 
 	tm_win_memclear(win);
 }
