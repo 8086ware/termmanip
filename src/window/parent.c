03cec7769bad4a0f04ad0f832907cb780af27c31
8086ware <kirillos.khan08@outlook.com>
2023-02-26 01:48:33 -0500

    Set child type in tm_win_parent

--- src/window/parent.c
+++ src/window/parent.c
@@ -2,8 +2,9 @@
 #include "exit_log.h"
 #include <stdlib.h>
 
-void tm_win_parent(Tm_window* parent, Tm_window* child) {
+void tm_win_parent(Tm_window* parent, Tm_window* child, int type) {
 	child->parent = parent;
+	child->child_type = type;
 	
 	parent->children = realloc(parent->children, (parent->children_amount + 1) * sizeof(Tm_window*));
 
@@ -12,6 +13,6 @@ void tm_win_parent(Tm_window* parent, Tm_window* child) {
 	}
 
 	parent->children[parent->children_amount] = child;
-
+	
 	parent->children_amount++;
 }
