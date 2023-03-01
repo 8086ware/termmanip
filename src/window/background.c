e20bb2acb35f66020be368b607e90679cb8f2b8f
8086ware <kirillos.khan08@outlook.com>
2023-02-28 20:56:56 -0500

    Add check for temp, use tm_win_clear to clear whole screen and apply background, and return a value for future errors

--- src/window/background.c
+++ src/window/background.c
@@ -2,25 +2,28 @@
 #include "append_win.h"
 #include <stdlib.h>
 #include <string.h>
+#include "exit_log.h"
 
-void tm_win_background(Tm_window* win, int attrib) {
+int tm_win_background(Tm_window* win, int attrib) {
 	char* temp = malloc(win->content_len * sizeof(char));
+
+	if(temp == NULL) {
+		exit_log("tm_win_background", "malloc", 1);
+	}
+	
 	int temp_len = win->content_len;
 	int cursor_x = win->cursor_x, cursor_y = win->cursor_y;	
+
 	strncpy(temp, win->contents, temp_len);
 
-	tm_win_memclear(win);
+	tm_win_attrib(win, attrib);
 
-	for(int y = 0; y < win->rows; y++) {
-		for(int x = 0; x < win->columns; x++) {
-			tm_win_cursor(win, x, y);
-			tm_win_attrib(win, attrib);
-			append_win(win, "%c", ' ');
-		}
-	}
+	tm_win_clear(win);	
 
 	append_win(win, temp);
 
 	win->cursor_x = cursor_x;
 	win->cursor_y = cursor_y;
+
+	return 0;
 }
