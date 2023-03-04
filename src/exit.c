9fbb72ee158bd6256a479a666ae105cd3a82f1da
8086ware <kirillos.khan08@outlook.com>
2023-03-03 22:03:43 -0500

    Enable echo and icanon (the default) after exiting termmanip

--- src/exit.c
+++ src/exit.c
@@ -2,11 +2,18 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <termios.h>
 
 void tm_exit() {
 	tm_win_free(default_win);
 	write(fileno(stdout), TM_ESC_LEAVE_ALT_SCREEN, strlen(TM_ESC_ENTER_ALT_SCREEN));
 	write(fileno(stdout), TM_ESC_ENABLE_CURSOR, strlen(TM_ESC_ENABLE_CURSOR));
-	tm_echo(1);
-	tm_rawinput(0);
+
+	struct termios term;
+	tcgetattr(fileno(stdin), &term);
+
+	term.c_lflag |= ECHO;
+	term.c_lflag |= ICANON;
+
+	tcsetattr(fileno(stdin), TCSANOW, &term);
 }
