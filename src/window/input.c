b25b578e97ddae45c210399498901ba8acca86e9
8086ware <kirillos.khan08@outlook.com>
2023-03-03 23:54:32 -0500

    Add backspace support by checking if ch == "\177" (DEL) and delete the previous character. Added continue to type characters even after you are beyond limit but wont add to str.

--- src/window/input.c
+++ src/window/input.c
@@ -24,9 +24,47 @@ void tm_win_input_str(Tm_window* win, char* str, int max_size) {
 	int i = 0;
 	char ch = 0;
 
-	while(i != max_size && ch != '\n') {
-		ch = tm_input_ch();
-		tm_win_print(win, "%c", ch);
+	int ret = 0;
+
+	while(ch != '\n') {	
+		read(fileno(stdin), &ch, 1);
+
+		if(ch == '\177') {
+			if(i != 0) {
+				str[i] = '\0';
+				i--;
+			}
+
+			else {
+				continue;
+			}
+
+			int tempX = win->cursor_x;
+			int tempY = win->cursor_y;
+
+			if((ret = tm_win_cursor(win, win->cursor_x - 1, win->cursor_y)) == TM_ERROR) {
+				return ret;
+			}
+
+			if((ret = append_win(win, " ")) == TM_ERROR) {
+				return ret;
+			}
+
+			if((ret = tm_win_cursor(win, tempX - 1, tempY)) == TM_ERROR) {
+				return ret;
+			}
+
+			tm_win_update(win);
+			continue;
+		}
+
+
+		if(i <= max_size) {
+			str[i] = ch;
+			i++;
+		}
+
+		tm_win_print(win, "%c", ch);	
 		tm_win_update(win);
 		str[i] = ch;
 		i++;
