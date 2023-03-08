dd9c5251857c3417e7d94cab77296ea0e8736e8f
8086ware <kirillos.khan08@outlook.com>
2023-03-07 21:48:25 -0500

    Remove include windows.h

--- src/window/echo.c
+++ src/window/echo.c
@@ -1,10 +1,6 @@
 #include "termmanip.h"
 #include <stdio.h>
-#ifdef _WIN32
-#include <windows.h>
-#else
 #include <termios.h>
-#endif
 
 void tm_win_echo(Tm_window* win, int state) {
 	if(state <= 0) {
