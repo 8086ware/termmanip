6a7cdf966187535e660f72edac41739c3f01142f
8086ware <kirillos.khan08@outlook.com>
2023-03-07 20:40:23 -0500

    Add new test

--- test/test.c
+++ test/test.c
@@ -1,26 +1,58 @@
 #include "termmanip.h"
+#include <time.h>
 
 int main(void) {
 	tm_init();
-	tm_background(TM_ATTRIB_BG_BLUE);
-	tm_attrib(TM_ATTRIB_FG_RED);
-	tm_cursor(1, 1);
-	tm_print("Hello World!\n%d%c%f", 10, 'c', 10.1);
-	tm_update();
-	tm_input_ch();
 
-	int i = tm_dialog(0, 0, 50, 30, "Hello World! Dialog test", 2, "Yes", "No");
-
-	if(i == 0) {
-		tm_print("Yes pressed\n");
+	for(int i = 0; i < 100000; i++) {
+		tm_cursor(0, 0);
+		tm_print("Loading... %d", i);
+		tm_update();
 	}
 
-	else {
-		tm_print("No pressed\n");
+	tm_cursor(10, 10);
+	tm_print("Done!");
+	tm_update();
+	sleep(2);
+
+	tm_clear();
+	tm_print("Hello World");
+	tm_clear();
+	tm_print("Hello... World?");
+	tm_fill(10, 10, 20, 20, 't', TM_ATTRIB_BG_BLUE);
+	tm_update();
+	char buffer[1024];
+	Tm_window* win = tm_window(40, 20, 10, 10);
+	tm_win_border(win);
+	tm_win_input_str(win, buffer, 1024);
+
+	tm_attrib(TM_ATTRIB_BG_RED);
+	int scr_x, scr_y;
+	tm_get_scrsize(&scr_x, &scr_y);
+
+	int i = 0;
+
+	while(i != 1000) {
+		tm_cursor(rand() % scr_x, rand() % scr_y);
+		tm_print("ERROR");
+		tm_update();
+		usleep(1000);
+		i++;
 	}
 
+	tm_attrib(TM_ATTRIB_BG_YELLOW);
+	tm_clear();
 	tm_update();
 
+	sleep(3);
+
+	tm_print("Welcome to termmanip. Enjoy your tui!");
+	tm_update();
+
+	tm_button(10, 10, 10, 10, "Of course");
+	tm_button(0, 5, 10, 10, "Heck No");
+
+	tm_button_select();
 	tm_input_ch();
 	tm_exit();
 }
