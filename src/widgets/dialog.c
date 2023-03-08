9157809478d7708eec253a7caa922d6f620779d8
8086ware <kirillos.khan08@outlook.com>
2023-03-07 21:46:38 -0500

    Rewrite dialog a bit

--- src/widgets/dialog.c
+++ src/widgets/dialog.c
@@ -3,47 +3,45 @@
 #include <stdarg.h>
 
 int tm_win_dialog(Tm_window* win, int x, int y, int columns, int rows, char* message, const int option_amount, ...) {
+	Tm_window* dialog = tm_window(x, y, columns, rows);
+	
+	tm_win_parent(win, dialog, TM_CHILD_NORMAL);
+
+	tm_win_border(dialog);
+
+	tm_win_cursor(dialog, columns / 2 - strlen(message) / 2, rows / 2);
+
+	tm_win_print(dialog, message);
+
 	va_list option_args;
+
+	va_start(option_args, option_amount);
 
 	if(option_amount == 0) {
 		tm_error_number = TM_DIALOG_NO_OPTIONS;
 		return TM_ERROR;
 	}
 
-	Tm_window* dialog = tm_window(x, y, columns, rows);
-
-	int option_columns = columns / option_amount;
-	int option_rows = 3;
 	int option_x = 0;
 	int option_y = rows - 4;
 
-	tm_win_parent(win, dialog, TM_CHILD_NORMAL);
-	tm_win_border(dialog);
-	tm_win_cursor(dialog, columns / 2 - strlen(message) / 2, rows / 2);
-	tm_win_print(dialog, message);
+	int option_columns = columns / option_amount;
+	int option_rows = 3;
 
 	Tm_window* buttons[option_amount];
 
-	va_start(option_args, option_amount);
-
-	char* options[option_amount];
-
 	for(int i = 0; i < option_amount; i++) {
-		options[i] = va_arg(option_args, char*);
-		buttons[i] = tm_win_button(dialog, option_x, option_y, option_columns, option_rows, options[i]);
 
-		option_x += columns / option_amount;
+		char* button_text = va_arg(option_args, char*);
+		buttons[i] = tm_win_button(dialog, option_x, option_y, option_columns, option_rows, button_text);
+		option_x += option_columns;
 	}
 
-	va_end(option_args);
 	tm_win_update(dialog);
-
-	Tm_window* selection = tm_win_button_select(dialog);
-
-	tm_win_clear(dialog);
+	Tm_window* button_selected = tm_win_button_select(dialog);
 
 	for(int i = 0; i < option_amount; i++) {
-		if(selection == buttons[i]) {
+		if(button_selected == buttons[i]) {
 			return i;
 		}
 	}
