818d62ae8d03df7870982e1e9667d46160c6f01e
8086ware <kirillos.khan08@outlook.com>
2023-03-09 07:32:44 -0500

    Initalize and deinitalize for windows for termmanip to work on windows too

--- src/exit.c
+++ src/exit.c
@@ -2,13 +2,41 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <termios.h>
+#endif
 
 void tm_exit() {
+	append_win(default_win, TM_ESC_ENABLE_CURSOR);
+	append_win(default_win, TM_ESC_LEAVE_ALT_SCREEN);
+	tm_update();
 	tm_win_free(default_win);
-	write(fileno(stdout), TM_ESC_LEAVE_ALT_SCREEN, strlen(TM_ESC_ENTER_ALT_SCREEN));
-	write(fileno(stdout), TM_ESC_ENABLE_CURSOR, strlen(TM_ESC_ENABLE_CURSOR));
+#ifdef _WIN32
+	DWORD mode = 0;
 
+	if(GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &mode) == 0) {
+		exit_log("tm_init", "SetConsoleMode", 1);
+	}
+
+	mode &= ~ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+	if(SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), mode) == 0) {
+		exit_log("tm_exit", "SetConsoleMode", 1);
+	}
+
+	if (GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), &mode) == 0) {
+		exit_log("tm_exit", "SetConsoleMode", 1);
+	}
+
+	mode |= ECHO_LINE_INPUT;
+	mode |= ENABLE_ECHO_INPUT;
+
+	if(SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), mode) == 0) {
+		exit_log("tm_exit", "SetConsoleMode", 1);
+	}
+#else
 	struct termios term;
 	tcgetattr(fileno(stdin), &term);
 
@@ -16,4 +44,5 @@ void tm_exit() {
 	term.c_lflag |= ICANON;
 
 	tcsetattr(fileno(stdin), TCSANOW, &term);
+#endif
 }
--- src/init.c
+++ src/init.c
@@ -5,11 +5,50 @@
 #include "exit_log.h"
 #include "append_win.h"
 #include "signal_handler.h"
+#include "exit_log.h"
 #include <signal.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <termios.h>
 #include <unistd.h>
+#endif
 
 void tm_init() {
+#ifdef _WIN32
+	DWORD mode = 0;
+
+	if(GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &mode) == 0) {
+		exit_log("tm_init", "GetConsoleMode", 1);
+	}
+
+	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+	if(SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), mode) == 0) {
+		exit_log("tm_init", "SetConsoleMode", 1);
+	}
+
+	if(GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), &mode) == 0) {
+		exit_log("tm_init", "GetConsoleMode", 1);
+	}
+
+	mode &= ~ENABLE_ECHO_INPUT;
+	mode &= ~ENABLE_LINE_INPUT;
+
+	if(SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), mode) == 0) {
+		exit_log("tm_init", "SetConsoleMode", 1);
+	}
+
+
+#else 
+	struct termios term;
+	tcgetattr(fileno(stdin), &term);
+
+	term.c_lflag &= ~ECHO;
+	term.c_lflag &= ~ICANON;
+
+	tcsetattr(fileno(stdin), TCSANOW, &term);
+#endif
 	signal(SIGINT, signal_handle);
 	signal(SIGABRT, signal_handle);
 	signal(SIGSEGV, signal_handle);
@@ -19,17 +58,32 @@ void tm_init() {
 	int scr_columns, scr_rows;
 
 	tm_get_scrsize(&scr_columns, &scr_rows);
-	default_win = tm_window(0, 0, scr_columns, scr_rows);
 
-	write(fileno(stdout), TM_ESC_ENTER_ALT_SCREEN, strlen(TM_ESC_ENTER_ALT_SCREEN));
-	write(fileno(stdout), "\x1b[2J", strlen("\x1b[2J"));
+	default_win = malloc(sizeof(Tm_window));
 
-	struct termios term;
-	tcgetattr(fileno(stdin), &term);
+	if(default_win == NULL) {
+		exit_log("tm_init", "malloc", 1);
+	}
 
-	term.c_lflag &= ~ECHO;
-	term.c_lflag &= ~ICANON;
+	tm_win_modify(default_win, 0, 0, scr_columns, scr_rows);
 
-	tcsetattr(fileno(stdin), TCSANOW, &term);
+	default_win->parent = NULL;
+	default_win->children = NULL;
+
+	default_win->children_amount = 0;
+
+	default_win->contents = NULL;
+	default_win->content_len = 0;
+
+	append_win(default_win, TM_ESC_ENTER_ALT_SCREEN);
+	append_win(default_win, "\x1b[2J");
+
+	tm_win_echo(default_win, 1);
+	tm_win_raw(default_win, 1);
+
+	tm_win_cursor(default_win, 0, 0);
+	tm_win_attrib(default_win, TM_ATTRIB_RESET);
+
+	tm_update();
 }
 
