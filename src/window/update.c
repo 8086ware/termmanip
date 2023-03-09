42aa17f2d89afe26975dd316a76e7262fc26831c
8086ware <kirillos.khan08@outlook.com>
2023-03-09 07:33:42 -0500

    Use WriteConsole on windows instead of write

--- src/window/update.c
+++ src/window/update.c
@@ -2,6 +2,10 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 void tm_win_update(Tm_window* win) {
 	if(win->children != NULL) {
 		for(int i = 0; i < win->children_amount; i++) {
@@ -9,7 +13,11 @@ void tm_win_update(Tm_window* win) {
 		}
 	}
 
+#ifdef _WIN32
+	DWORD bytes_written = 0;
+	WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), win->contents, win->content_len, &bytes_written, NULL);
+#else
 	write(fileno(stdout), win->contents, win->content_len);
-
+#endif
 	tm_win_memclear(win);
 }
