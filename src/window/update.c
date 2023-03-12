876eef65fe779403e30729b6e55a6c22a86eb4c0
8086ware <kirillos.khan08@outlook.com>
2023-03-11 21:00:53 -0500

    Update children after updating parent window

--- src/window/update.c
+++ src/window/update.c
@@ -7,17 +7,18 @@
 #endif
 
 void tm_win_update(Tm_window* win) {
-	if(win->children != NULL) {
-		for(int i = 0; i < win->children_amount; i++) {
-			tm_win_update(win->children[i]);
-		}
-	}
-
 #ifdef _WIN32
 	DWORD bytes_written = 0;
 	WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), win->contents, win->content_len, &bytes_written, NULL);
 #else
 	write(fileno(stdout), win->contents, win->content_len);
 #endif
+
+	if(win->children != NULL) {
+		for(int i = 0; i < win->children_amount; i++) {
+			tm_win_update(win->children[i]);
+		}
+	}
+
 	tm_win_memclear(win);
 }
