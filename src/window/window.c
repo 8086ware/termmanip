0c0f615c75ccd6ef51c4270a44831c634f6f3c12
8086ware <kirillos.khan08@outlook.com>
2023-03-19 16:13:06 -0400

    Return return value of tm_win_modify if failure

--- src/window/window.c
+++ src/window/window.c
@@ -6,13 +6,17 @@
 Tm_window* default_win = NULL;
 
 Tm_window* tm_window(int x, int y, int columns, int rows) {
+	int ret = 0;
+
 	Tm_window* win = malloc(sizeof(Tm_window));
 
 	if(win == NULL) {
 		exit_log("tm_window", "malloc", 1);
 	}
 
-	tm_win_modify(win, x, y, columns, rows);
+	if((ret = tm_win_modify(win, x, y, columns, rows)) == TM_ERROR) {
+		return ret;
+	}
 
 	win->parent = NULL;
 	win->children = NULL;
