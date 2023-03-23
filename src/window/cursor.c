c8af55b30291e7a60db820126a63fb47b422d6a0
8086ware <kirillos.khan08@outlook.com>
2023-03-22 23:55:24 -0400

    Remove the old tm_win_cursor

--- src/window/cursor.c
+++ src/window/cursor.c
@@ -16,24 +16,6 @@ int tm_win_cursor(Tm_window* win, int x, int y) {
 	return 0;
 }
 
-int tm_win_cursor(Tm_window* win, int x, int y) {
-	int ret = 0;
-
-	if((ret = tm_win_cursor_internal(win, x, y)) == TM_ERROR) {
-		return ret;
-	}
-
-	int parent_x = 0;
-	int parent_y = 0;
-
-	if(win->parent != NULL) {
-		parent_x = win->parent->position_x;
-		parent_y = win->parent->position_y;
-	}
-
-	return append_win(win, "\x1b[%d;%dH", parent_y + win->cursor_y + win->position_y + 1, parent_x + win->cursor_x + win->position_x + 1);
-}
-
 int tm_win_cursor_visible(Tm_window* win, int state) {
 	if(state <= 0) {
 		return append_win(win, TM_ESC_DISABLE_CURSOR);
