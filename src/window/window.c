0f4c0ccc309afbdacdf980dfc8fb46c5ab9d98fc
8086ware <kirillos.khan08@outlook.com>
2023-03-23 16:48:12 -0400

    Set all the values on its own

--- src/window/window.c
+++ src/window/window.c
@@ -14,10 +14,15 @@ Tm_window* tm_window(int x, int y, int columns, int rows) {
 		exit_log("tm_window", "malloc", 1);
 	}
 
-	if((ret = tm_win_modify(win, x, y, columns, rows)) == TM_ERROR) {
-		return ret;
-	}
+	win->columns = columns;
+	win->rows = rows;
 
+	win->position_x = x;
+	win->position_y = y;
+	
+	win->cursor_x = 0;
+	win->cursor_y = 0;
+	
 	win->parent = NULL;
 	win->children = NULL;
 
@@ -29,12 +34,17 @@ Tm_window* tm_window(int x, int y, int columns, int rows) {
 	win->flags = 0;
 
 	win->cursor_visible = 1;
-	
+	win->cursor_moved = 0;
+
 	win->attrib = TM_ATTRIB_RESET;
 
 	win->buffer = malloc(sizeof(Tm_char) * win->columns * win->rows);
 	win->physical_buffer = malloc(sizeof(Tm_char) * win->columns * win->rows);
 
+	if(win->buffer == NULL || win->physical_buffer == NULL) {
+		exit_log("tm_window", "malloc", 2);
+	}
+	
 	for(int i = 0; i < win->columns * win->rows; i++) {
 		win->buffer[i].disp = '\0';
 		win->buffer[i].attrib = TM_ATTRIB_RESET;
@@ -42,14 +52,8 @@ Tm_window* tm_window(int x, int y, int columns, int rows) {
 		win->physical_buffer[i].attrib = TM_ATTRIB_RESET;
 	}
 
-	if(win->buffer == NULL || win->physical_buffer == NULL) {
-		exit_log("tm_window", "malloc", 2);
-	}
-	
 	tm_win_echo(win, 1);
 	tm_win_raw(win, 1);
-
-	tm_win_cursor(win, 0, 0);
 
 	return win;
 }
@@ -64,8 +68,13 @@ void tm_win_free(Tm_window* win) {
 	}
 
 	free(win->children);
+	win->children = NULL;
 	free(win->output);
+	win->output = NULL;
 	free(win->buffer);
+	win->buffer = NULL;
 	free(win->physical_buffer);
+	win->physical_buffer = NULL;
 	free(win);
+	win = NULL;
 }
