3b193959f7db093a2484666b1c2825f5b0dffe28
8086ware <kirillos.khan08@outlook.com>
2023-03-22 23:53:24 -0400

    Check window cursor for error, and put char in the whole window with spaces and TM_ATTRIB_RESET. Also clear the children after

--- src/window/clear.c
+++ src/window/clear.c
@@ -1,18 +1,26 @@
 #include "termmanip.h"
-#include "append_win.h"
 
 int tm_win_clear(Tm_window* win) {
 	int ret = 0;
-	tm_win_cursor(win, 0, 0);
-
-	for(int y = 0; y < win->rows; y++) {
-		for(int x = 0; x < win->columns; x++) {
-			tm_win_print(win, " ");
-		}
-	}
 
 	if((ret = tm_win_cursor(win, 0, 0)) == TM_ERROR) {
 		return ret;
+	}
+	
+	for(int y = 0; y < win->rows; y++) {
+		for(int x = 0; x < win->columns; x++) {
+			tm_win_putch(win, x, y, ' ', TM_ATTRIB_RESET);
+		}
+	}
+
+	tm_win_attrib(win, TM_ATTRIB_RESET);
+
+	if((ret = tm_win_cursor(win, 0, 0)) == TM_ERROR) {
+		return ret;
+	}
+
+	for(int i = 0; i < win->children_amount; i++) {
+		tm_win_clear(win->children[i]);
 	}
 
 	return 0;
