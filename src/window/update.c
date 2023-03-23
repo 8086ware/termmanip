c66c4516f417a8c25ca9a12ce98c7ab236c37da1
8086ware <kirillos.khan08@outlook.com>
2023-03-23 00:46:03 -0400

    Rewrite update function. The update function loops through the whole window and compares the window buffer on screen to the one pending. If changes are found, set the cursor to the x,y where the change occurs, and set still_writing on. still_writing is there so it doesnt need to set the cursor every single time it finds a change. Check if the last attribute found which may be 0 is not the same as the one in the pending buffer in x,y, if not, go ahead and print the proper escape code to match the attribute and set the last_attrib to it. Then append the character to the output. If changes are not found, then put still_writing to 0 and do nothing. Put still_writing = 0 every time a new row is entered aswell. After that, the cursor is set in the terminal to the window cursor x, y.

--- src/window/update.c
+++ src/window/update.c
@@ -1,12 +1,115 @@
 #include "termmanip.h"
 #include <unistd.h>
 #include <stdio.h>
+#include "append_output.h"
+#include <stdlib.h>
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
 void tm_win_update(Tm_window* win) {
+	int parent_x = 0;
+	int parent_y = 0;
+
+	if(win->parent != NULL) {
+		parent_x = win->parent->position_x;
+		parent_y = win->parent->position_y;			
+	}
+
+	static uint32_t last_attrib;
+	static _Bool still_writing = 0;
+
+	_Bool found = 0;
+	for(int y = 0; y < win->rows; y++) {
+		for(int x = 0; x < win->columns; x++) {
+			char phys_disp = win->physical_buffer[y * win->columns + x].disp;
+			char disp = win->buffer[y * win->columns + x].disp;
+			uint32_t phys_attrib = win->physical_buffer[y * win->columns + x].attrib;
+			uint32_t attrib = win->buffer[y * win->columns + x].attrib;
+
+			if(phys_disp != disp || phys_attrib != attrib) {
+				found = 1;
+
+				if(still_writing == 0) {
+					append_output(win, "\x1b[%d;%dH", y + win->position_y + 1 + parent_y, x + win->position_x + 1 + parent_x);
+					still_writing = 1;
+				}
+
+				if(last_attrib != attrib) {
+					last_attrib = attrib;
+
+					if(attrib & TM_ATTRIB_BOLD) {
+						append_output(win, "\x1b[1m");
+					}
+
+					if(attrib & TM_ATTRIB_DIM) {
+						append_output(win, "\x1b[2m");
+					}
+
+					if(attrib & TM_ATTRIB_ITALIC) {
+						append_output(win, "\x1b[3m");
+					}
+
+					if(attrib & TM_ATTRIB_UNDERLINE) {
+						append_output(win, "\x1b[4m");
+					}
+
+					if(attrib & TM_ATTRIB_BLINKING) {
+						append_output(win, "\x1b[5m");
+					}
+
+					if(attrib & TM_ATTRIB_HIGHLIGHT) {
+						append_output(win, "\x1b[7m");
+					}
+
+					if(attrib & TM_ATTRIB_HIDDEN) {
+						append_output(win, "\x1b[8m");
+					}
+
+					if(attrib & TM_ATTRIB_STRIKE) {
+						append_output(win, "\x1b[9m");
+					}
+
+					if(attrib & TM_ATTRIB_ALTERNATE) {
+						append_output(win, "\x1b(0");
+					}
+
+					if(attrib & TM_ATTRIB_MAIN) {
+						append_output(win, "\x1b(B");
+					}
+
+					if((attrib & TM_ATTRIB_FG_MASK) != 0) {
+						append_output(win, "\x1b[%dm", (attrib & TM_ATTRIB_FG_MASK) >> 16);
+					}
+
+					if((attrib & TM_ATTRIB_BG_MASK) != 0) {
+						append_output(win, "\x1b[%dm", (attrib & TM_ATTRIB_BG_MASK) >> 24);
+					}
+
+					if(attrib & TM_ATTRIB_RESET) {
+						append_output(win, "\x1b[0m");
+					}
+				}
+
+				if(disp == '\0') {
+					disp = ' ';
+				}
+
+				append_output(win, "%c", disp);
+			}
+
+			else {
+				still_writing = 0;
+			}		
+		}
+
+		still_writing = 0;
+	}
+
+	append_output(win, "\x1b[%d;%dH", win->cursor_y + win->position_y + 1 + parent_y, win->cursor_x + win->position_x + 1 + parent_x);
+
+	if(found) {
 #ifdef _WIN32
 	DWORD bytes_written = 0;
 	WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), win->contents, win->content_len, &bytes_written, NULL);
