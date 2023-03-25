f5482f792b3e19567a00c709cbd1d65271403e6c
8086ware <kirillos.khan08@outlook.com>
2023-03-25 02:20:47 -0400

    Revert "Update the window update method. It creates a new dynamic array and detects and fills it with pending changes, and then those get written to the screen. Set cursor moved to 0 after updating"
    This reverts commit d6efb89758c815657bdea3067cfe85733ea4b0aa.

--- src/window/update.c
+++ src/window/update.c
@@ -26,10 +26,9 @@ void tm_win_update(Tm_window* win) {
 	}
 
 	static uint32_t last_attrib;
+	static _Bool still_writing = 0;
+
 	_Bool found = 0;
-
-	int pending_changes_amount = 0;
-
 	for(int y = 0; y < win->rows; y++) {
 		for(int x = 0; x < win->columns; x++) {
 			char phys_disp = win->physical_buffer[y * win->columns + x].disp;
@@ -39,121 +38,84 @@ void tm_win_update(Tm_window* win) {
 
 			if(phys_disp != disp || phys_attrib != attrib) {
 				found = 1;
-				pending_changes_amount++;
-			}
-		}
-	}
 
-	Pending_Change* pending_changes = malloc(sizeof(Pending_Change) * pending_changes_amount);
+				if(still_writing == 0) {
+					append_output(win, "\x1b[%d;%dH", y + win->position_y + 1 + parent_y, x + win->position_x + 1 + parent_x);
+					still_writing = 1;
+				}
 
-	int i = 0;
-	for(int y = 0; y < win->rows; y++) {
-		for(int x = 0; x < win->columns; x++) {
-			char phys_disp = win->physical_buffer[y * win->columns + x].disp;
-			char disp = win->buffer[y * win->columns + x].disp;
-			uint32_t phys_attrib = win->physical_buffer[y * win->columns + x].attrib;
-			uint32_t attrib = win->buffer[y * win->columns + x].attrib;
+				if(last_attrib != attrib) {
+					last_attrib = attrib;
 
-			if(phys_disp != disp || phys_attrib != attrib) {
-				pending_changes[i].changed_xy = &win->buffer[y * win->columns + x];
-				pending_changes[i].x = x;
-				pending_changes[i].y = y;
-				pending_changes[i].move = 0;
-
-				if(i > 0) {
-					if(pending_changes[i].y > pending_changes[i - 1].y) {
-						pending_changes[i].move = 1;
+					if(attrib & TM_ATTRIB_BOLD) {
+						append_output(win, "\x1b[1m");
 					}
 
-					if((pending_changes[i].x - pending_changes[i - 1].x) > 5) {
-						pending_changes[i].move = 1;
+					if(attrib & TM_ATTRIB_DIM) {
+						append_output(win, "\x1b[2m");
+					}
+
+					if(attrib & TM_ATTRIB_ITALIC) {
+						append_output(win, "\x1b[3m");
+					}
+
+					if(attrib & TM_ATTRIB_UNDERLINE) {
+						append_output(win, "\x1b[4m");
+					}
+
+					if(attrib & TM_ATTRIB_BLINKING) {
+						append_output(win, "\x1b[5m");
+					}
+
+					if(attrib & TM_ATTRIB_HIGHLIGHT) {
+						append_output(win, "\x1b[7m");
+					}
+
+					if(attrib & TM_ATTRIB_HIDDEN) {
+						append_output(win, "\x1b[8m");
+					}
+
+					if(attrib & TM_ATTRIB_STRIKE) {
+						append_output(win, "\x1b[9m");
+					}
+
+					if(attrib & TM_ATTRIB_ALTERNATE) {
+						append_output(win, "\x1b(0");
+					}
+
+					if(attrib & TM_ATTRIB_MAIN) {
+						append_output(win, "\x1b(B");
+					}
+
+					if((attrib & TM_ATTRIB_FG_MASK) != 0) {
+						append_output(win, "\x1b[%dm", (attrib & TM_ATTRIB_FG_MASK) >> 16);
+					}
+
+					if((attrib & TM_ATTRIB_BG_MASK) != 0) {
+						append_output(win, "\x1b[%dm", (attrib & TM_ATTRIB_BG_MASK) >> 24);
+					}
+
+					if(attrib & TM_ATTRIB_RESET) {
+						append_output(win, "\x1b[0m");
 					}
 				}
 
-				i++;
+				if(disp == '\0') {
+					disp = ' ';
+				}
+
+				append_output(win, "%c", disp);
 			}
+
+			else {
+				still_writing = 0;
+			}		
 		}
+
+		still_writing = 0;
 	}
 
-	append_output(win, "\x1b[%d;%dH", pending_changes[0].y + win->position_y + 1 + parent_y, win->position_x + pending_changes[0].x + 1 + parent_x);
-
-	for(int i = 0; i < pending_changes_amount; i++) {
-		uint32_t attrib = pending_changes[i].changed_xy->attrib;
-		char disp = pending_changes[i].changed_xy->disp;
-
-		if(pending_changes[i].move) {
-			append_output(win, "\x1b[%d;%dH", pending_changes[i].y + win->position_y + 1 + parent_y, win->position_x + pending_changes[i].x + 1 + parent_x);
-		}
-
-		if(last_attrib != attrib) {
-			last_attrib = attrib;
-
-			if(attrib & TM_ATTRIB_BOLD) {
-				append_output(win, "\x1b[1m");
-			}
-
-			if(attrib & TM_ATTRIB_DIM) {
-				append_output(win, "\x1b[2m");
-			}
-
-			if(attrib & TM_ATTRIB_ITALIC) {
-				append_output(win, "\x1b[3m");
-			}
-
-			if(attrib & TM_ATTRIB_UNDERLINE) {
-				append_output(win, "\x1b[4m");
-			}
-
-			if(attrib & TM_ATTRIB_BLINKING) {
-				append_output(win, "\x1b[5m");
-			}
-
-			if(attrib & TM_ATTRIB_HIGHLIGHT) {
-				append_output(win, "\x1b[7m");
-			}
-
-			if(attrib & TM_ATTRIB_HIDDEN) {
-				append_output(win, "\x1b[8m");
-			}
-
-			if(attrib & TM_ATTRIB_STRIKE) {
-				append_output(win, "\x1b[9m");
-			}
-
-			if(attrib & TM_ATTRIB_ALTERNATE) {
-				append_output(win, "\x1b(0");
-			}
-
-			if(attrib & TM_ATTRIB_MAIN) {
-				append_output(win, "\x1b(B");
-			}
-
-			if((attrib & TM_ATTRIB_FG_MASK) != 0) {
-				append_output(win, "\x1b[%dm", (attrib & TM_ATTRIB_FG_MASK) >> 16);
-			}
-
-			if((attrib & TM_ATTRIB_BG_MASK) != 0) {
-				append_output(win, "\x1b[%dm", (attrib & TM_ATTRIB_BG_MASK) >> 24);
-			}
-
-			if(attrib & TM_ATTRIB_RESET) {
-				append_output(win, "\x1b[0m");
-			}
-		}
-
-		if(disp == '\0') {
-			disp = ' ';
-		}
-
-		append_output(win, "%c", disp);
-	}
-
-	free(pending_changes);
-
-	if(win->cursor_moved) {
-		append_output(win, "\x1b[%d;%dH", win->cursor_y + win->position_y + 1 + parent_y, win->position_x + win->cursor_x + 1 + parent_x);
-		win->cursor_moved = 0;
-	}
+	append_output(win, "\x1b[%d;%dH", win->cursor_y + win->position_y + 1 + parent_y, win->cursor_x + win->position_x + 1 + parent_x);
 
 	if(found) {
 #ifdef _WIN32
@@ -176,4 +138,5 @@ void tm_win_update(Tm_window* win) {
 	for(int i = 0; i < win->children_amount; i++) {
 		tm_win_update(win->children[i]);
 	}
+
 }
