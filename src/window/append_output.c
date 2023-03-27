710564216a28192485e7fe7e6f52b2a69b44f325
8086ware <kirillos.khan08@outlook.com>
2023-03-27 14:39:14 -0400

    Output will be in the main screen, not in any windows, so change append_output for that

--- src/window/append_output.c
+++ src/window/append_output.c
@@ -6,7 +6,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-int append_output(Tm_window* win, char* fmt, ...) {
+void append_output(char* fmt, ...) {
 	va_list args;
 	va_start(args, fmt);
 	char buffer[strlen(fmt) + 2048];
@@ -14,18 +14,16 @@ int append_output(Tm_window* win, char* fmt, ...) {
 
 	va_end(args);
 	int len = strlen(buffer);
-	win->output = realloc(win->output, win->output_len + len);
+	screen->output = realloc(screen->output, screen->output_len + len);
 
-	if(win->output == NULL) {
+	if(screen->output == NULL) {
 		exit_log("append_output", "realloc", 1);
 	}
 
 	for(int i = 0; i < len; i++) {
-		win->output[win->output_len + i] = buffer[i];
+		screen->output[screen->output_len + i] = buffer[i];
 	}
 
-	win->output_len += len;
-
-	return 0;
+	screen->output_len += len;
 }
 
