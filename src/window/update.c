d5a61ee46c9e74a888c9a9a9b3e08560335b4439
8086ware <kirillos.khan08@outlook.com>
2023-03-27 20:06:59 -0400

    Loop through all pending changes, move to the position of their x,y as efficient as possible, and set the screen cursor x and y there. Then write the output to screen and free the pending changes and screen output

--- src/window/update.c
+++ src/window/update.c
@@ -27,16 +27,95 @@ void terminal_move_cursor(int x, int y) {
 	else {
 		append_output("\x1b[%d;%dH", y + 1, x + 1);
 	}
+}
 
-	append_output(win, "\x1b[%d;%dH", win->cursor_y + win->position_y + 1 + parent_y, win->cursor_x + win->position_x + 1 + parent_x);
+void tm_screen_update() {
+	for(int i = 0; i < screen->pending_change_amount; i++) {	
+		terminal_move_cursor(screen->pending_changes[i].x, screen->pending_changes[i].y);
 
-	if(found) {
+		char disp = screen->pending_changes[i].tm_char.disp;
+		uint32_t attrib = screen->pending_changes[i].tm_char.attrib;
+
+		if(screen->attrib != attrib) {
+			screen->attrib = attrib;
+
+			if(attrib & TM_ATTRIB_BOLD) {
+				append_output("\x1b[1m");
+			}
+
+			if(attrib & TM_ATTRIB_DIM) {
+				append_output("\x1b[2m");
+			}
+
+			if(attrib & TM_ATTRIB_ITALIC) {
+				append_output("\x1b[3m");
+			}
+
+			if(attrib & TM_ATTRIB_UNDERLINE) {
+				append_output("\x1b[4m");
+			}
+
+			if(attrib & TM_ATTRIB_BLINKING) {
+				append_output("\x1b[5m");
+			}
+
+			if(attrib & TM_ATTRIB_HIGHLIGHT) {
+				append_output("\x1b[7m");
+			}
+
+			if(attrib & TM_ATTRIB_HIDDEN) {
+				append_output("\x1b[8m");
+			}
+
+			if(attrib & TM_ATTRIB_STRIKE) {
+				append_output("\x1b[9m");
+			}
+
+			if(attrib & TM_ATTRIB_ALTERNATE) {
+				append_output("\x1b(0");
+			}
+
+			if(attrib & TM_ATTRIB_MAIN) {
+				append_output("\x1b(B");
+			}
+
+			if((attrib & TM_ATTRIB_FG_MASK) != 0) {
+				append_output("\x1b[%dm", (attrib & TM_ATTRIB_FG_MASK) >> 16);
+			}
+
+			if((attrib & TM_ATTRIB_BG_MASK) != 0) {
+				append_output("\x1b[%dm", (attrib & TM_ATTRIB_BG_MASK) >> 24);
+			}
+
+			if(attrib & TM_ATTRIB_RESET) {
+				append_output("\x1b[0m");
+			}
+		}
+
+		if(disp == '\0') {
+			disp = ' ';
+		}
+
+		append_output("%c", disp);
+
+		screen->cursor_x = screen->pending_changes[i].x;
+		screen->cursor_y = screen->pending_changes[i].y;
+	}
+
 #ifdef _WIN32
-		DWORD bytes_written = 0;
-		WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), win->output, win->output_len, &bytes_written, NULL);
+	DWORD bytes_written = 0;
+	WriteConsole(GetStdHandle(STD_OUTPUT_HANDLE), screen->output, screen->output_len, &bytes_written, NULL);
 #else
-		write(fileno(stdout), win->output, win->output_len);
+	write(fileno(stdout), screen->output, screen->output_len);
 #endif
+
+	free(screen->output);
+	screen->output = NULL;
+	screen->output_len = 0;
+
+	tm_free_pending_changes();
+}
+
 	}
 
 	free(win->output);
