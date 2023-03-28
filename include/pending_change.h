b1628ad0fbe12a5ab24556419530729f9f167d21
8086ware <kirillos.khan08@outlook.com>
2023-03-27 22:33:15 -0400

    Fix incorrect prototypes

--- include/pending_change.h
+++ include/pending_change.h
@@ -1,7 +1,7 @@
 #ifndef PENDING_CHANGE_H
 #define PENDING_CHANGE_H
 
-void tm_win_add_pending_change(Tm_window* win, int x, int y);
-void tm_win_delete_pending_changes(Tm_window* win);
+void tm_add_pending_change(Tm_window* win, int x, int y);
+void tm_delete_pending_changes(Tm_window* win);
 
 #endif
