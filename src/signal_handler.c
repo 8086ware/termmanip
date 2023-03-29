7cb18050bd52e2b90568ae502ba21cb5b39e2998
8086ware <kirillos.khan08@outlook.com>
2023-03-28 21:29:59 -0400

    Free pending changes when resizing

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -1,12 +1,14 @@
 #include <signal.h>
 #include "termmanip.h"
 #include <stdlib.h>
+#include "pending_change.h"
 
 void signal_handle(int sig) {
 #ifdef _WIN32
 #else
 	if(sig == SIGWINCH) {
 		int scr_x, scr_y;
+		tm_free_pending_changes();
 		tm_get_scrsize(&scr_x, &scr_y);
 		tm_win_modify(default_win, 0, 0, scr_x, scr_y);
 		tm_win_clear(default_win);
