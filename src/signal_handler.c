970504262469a699ec143f5666f8918673d4c907
8086ware <kirillos.khan08@outlook.com>
2023-04-03 23:25:00 -0400

    Remove call to tm_free_pending_changes

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -8,7 +8,6 @@ void signal_handle(int sig) {
 #else
 	if(sig == SIGWINCH) {
 		int scr_x, scr_y;
-		tm_free_pending_changes();
 		tm_get_scrsize(&scr_x, &scr_y);
 		tm_win_modify(default_win, 0, 0, scr_x, scr_y);
 		tm_win_clear(default_win);
