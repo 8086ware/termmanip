fb55f574564e8ae95fd61a709d5d4a5b6522ca31
8086ware <kirillos.khan08@outlook.com>
2023-04-10 18:24:45 -0400

    Use 0 instead of TM_ATTRIB_RESET and turn off TM_ATTRIB_ALL which sets attrib to 0

--- src/window/clear.c
+++ src/window/clear.c
@@ -9,11 +9,11 @@ int tm_win_clear(Tm_window* win) {
 	
 	for(int y = 0; y < win->rows; y++) {
 		for(int x = 0; x < win->columns; x++) {
-			tm_win_putch(win, x, y, ' ', TM_ATTRIB_RESET);
+			tm_win_putch(win, x, y, ' ', 0);
 		}
 	}
 
-	tm_win_attrib(win, TM_ATTRIB_RESET);
+	tm_win_attrib(win, TM_ATTRIB_ALL, 0);
 
 	if((ret = tm_win_cursor(win, 0, 0)) == TM_ERROR) {
 		return ret;
