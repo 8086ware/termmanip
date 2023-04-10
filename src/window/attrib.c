45065fe76bf00a916a33813ad3fd83f1b243ed13
8086ware <kirillos.khan08@outlook.com>
2023-04-10 18:21:08 -0400

    Instead of assigning win->attrib attrib, OR it into win->attrib if state is over 1 and AND it out if state is 0.

--- src/window/attrib.c
+++ src/window/attrib.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void tm_win_attrib(Tm_window* win, uint32_t attrib) {
-	win->attrib = attrib;
+void tm_win_attrib(Tm_window* win, uint32_t attrib, int state) {
+	if(state <= 0) {
+		win->attrib &= ~attrib;
+	}
+
+	else if(state > 0) {	
+		win->attrib |= attrib;
+	}
 }
