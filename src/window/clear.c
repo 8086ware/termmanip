6b86392e70c9b67a3ec69da066946cb6847ab106
8086ware <kirillos.khan08@outlook.com>
2023-04-30 00:44:57 -0400

    Make tm_win_clear clear the window with the window background tm char and set the attribute to it

--- src/window/clear.c
+++ src/window/clear.c
@@ -9,12 +9,13 @@ int tm_win_clear(Tm_window* win) {
 	
 	for(int y = 0; y < win->rows; y++) {
 		for(int x = 0; x < win->columns; x++) {
-			tm_win_putch(win,' ', 0);
+			tm_win_putch(win, win->background_tm_char.disp, win->background_tm_char.attrib);
 		}
 	}
 
 	tm_win_attrib(win, TM_ATTRIB_ALL, 0);
 
+	tm_win_attrib(win, win->background_tm_char.attrib, 1);
 	if((ret = tm_win_cursor(win, 0, 0)) == TM_ERROR) {
 		return ret;
 	}
