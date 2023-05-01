792670fe1116ee9246f930a99c03f14dcbea6497
8086ware <kirillos.khan08@outlook.com>
2023-04-30 23:12:17 -0400

    Instead of just clearing also keep the already written things in the window and only overwrite the attribute.

--- src/window/background.c
+++ src/window/background.c
@@ -5,11 +5,33 @@
 
 int tm_win_background(Tm_window* win, char ch, uint32_t attrib) {
 	int ret = 0;
+	int temp_cursor_x = win->cursor_x, temp_cursor_y = win->cursor_y;
+
+	if((ret = tm_win_cursor(win, 0, 0)) == TM_ERROR) {
+		return ret;
+	}
+	
+	for(int y = 0; y < win->rows; y++) {
+		for(int x = 0; x < win->columns; x++) {
+			if(win->buffer[y * win->columns + x].disp != win->background_tm_char.disp) {
+				tm_win_putch(win, win->buffer[y * win->columns + x].disp, attrib | win->buffer[y * win->columns + x].attrib); 
+			}
+
+			else {
+				tm_win_putch(win, ch, attrib); 
+			}
+		}
+	}
 
 	win->background_tm_char.attrib = attrib;
 	win->background_tm_char.disp = ch;
 
-	tm_win_clear(win);
+	tm_win_attrib(win, TM_ATTRIB_ALL, 0);
+	tm_win_attrib(win, win->background_tm_char.attrib, 1);
+
+	if((ret = tm_win_cursor(win, temp_cursor_x, temp_cursor_y)) == TM_ERROR) {
+		return ret;
+	}
 
 	return 0;
 }
