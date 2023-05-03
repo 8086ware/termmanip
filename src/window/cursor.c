6eaaab1a6177c8ac566d52f1c27d8fdf999e2f56
8086ware <kirillos.khan08@outlook.com>
2023-05-03 00:59:08 -0400

    Set win->cursor_x and win->cursor_y to x and y

--- src/window/cursor.c
+++ src/window/cursor.c
@@ -19,8 +19,8 @@ int tm_win_cursor(Tm_window* win, int x, int y) {
 		return TM_ERROR;
 	}
 
-	win->cursor_x = i % win->columns;
-	win->cursor_y = i / win->columns;	
+	win->cursor_x = x;
+	win->cursor_y = y;
 
 	return 0;
 }
@@ -34,7 +34,7 @@ int tm_win_cursor_visible(Tm_window* win, int state) {
 		win->flags |= TM_FLAG_CURSOR_VISIBLE;
 	}
 
-	
+
 	return 0;
 }
 
