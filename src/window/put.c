e51ee97adbee149f0bf3ab59d0107152bba0295f
8086ware <kirillos.khan08@outlook.com>
2023-05-03 01:00:23 -0400

    Make tm_win_putch use tm_win_cursor. Since tm_win_cursor handles scrolling remove scrolling code. Check for errors too.

--- src/window/put.c
+++ src/window/put.c
@@ -1,40 +1,23 @@
 #include "termmanip.h" 
 #include <string.h>
 
-int tm_win_putch(Tm_window* win, char ch, uint32_t attrib) {	
-	int position = win->cursor_y * win->columns + win->cursor_x;
-
-	if(position < 0 || position >= win->columns * win->rows) {
-		if(win->flags & TM_FLAG_SCROLL) {
-			if(position >= win->columns * win->rows) {
-				tm_win_scroll(win, (1 + (position / win->columns) - win->rows), TM_SCROLL_DOWN);	
-			}
-		}
-
-		else {
-			tm_error_number = TM_INVALID_CURSOR;		
-			return TM_ERROR;
-		}
-
-		position = win->cursor_y * win->columns + win->cursor_x;
-	}
+int tm_win_putch(Tm_window* win, char ch, uint32_t attrib) {
+	int ret = 0;
 
 	if(ch == '\n') {
-		win->cursor_y++;
-		win->cursor_x = 0;
+		if((ret = tm_win_cursor(win, 0, win->cursor_y + 1)) == TM_ERROR) {
+			return ret;
+		}
 	}
 
 	else {
-		win->buffer[position].attrib = attrib;
-		win->buffer[position].disp = ch;
+		win->buffer[win->cursor_y * win->columns + win->cursor_x].attrib = attrib;
+		win->buffer[win->cursor_y * win->columns + win->cursor_x].disp = ch;
 
-		win->cursor_x++;
+		if((ret = tm_win_cursor(win, win->cursor_x + 1, win->cursor_y)) == TM_ERROR) {
+			return ret;
+		}
 	}
-
-	position = win->cursor_y * win->columns + win->cursor_x;
-
-	win->cursor_x = position % win->columns;
-	win->cursor_y = position / win->columns;
 
 	return 0;
 }
