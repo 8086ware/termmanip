f3173f071782b43dc57435b68e94ce5149b371ff
8086ware <kirillos.khan08@outlook.com>
2023-05-05 06:05:56 +0100

    Use win->buffer_columns instead

--- src/window/put.c
+++ src/window/put.c
@@ -11,8 +11,8 @@ int tm_win_putch(Tm_window* win, char ch, uint32_t attrib) {
 	}
 
 	else {
-		win->buffer[win->cursor_y * win->columns + win->cursor_x].attrib = attrib;
-		win->buffer[win->cursor_y * win->columns + win->cursor_x].disp = ch;
+		win->buffer[win->cursor_y * win->buffer_columns + win->cursor_x].attrib = attrib;
+		win->buffer[win->cursor_y * win->buffer_columns + win->cursor_x].disp = ch;
 
 		if((ret = tm_win_cursor(win, win->cursor_x + 1, win->cursor_y)) == TM_ERROR) {
 			return ret;
