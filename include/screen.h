bea5e76eec8364c8996396a08780cc5edc3ef85c
8086ware <kirillos.khan08@outlook.com>
2023-05-16 01:32:42 -0400

    Screen.h

--- include/screen.h
+++ include/screen.h
@@ -1,6 +1,19 @@
 #ifndef SCREEN_H
 #define SCREEN_H
-	
+
+#include "termmanip.h"
+#include <stdint.h>
+
 void screen_init();
+void screen_free();
+
+int screen_cursor(int x, int y);
+
+void screen_attrib(uint32_t attrib);
+
+void screen_output_write(int x, int y, char ch, uint32_t attrib);
+void screen_append_output(char* fmt, ...);
+
+int screen_buffer_write(int x, int y, Tm_char ch);
 
 #endif
