ecae9d7cdadc6a42f0fc422c005071775e722d97
8086ware <kirillos.khan08@outlook.com>
2023-05-23 19:19:04 -0400

    Instead of using tm_win_cursor manipulate the cursor x and y manually in tm_win_putch and detect scrolling to fix problems caused by tm_win_cursor (scrolling glitches)

--- src/window/put.c
+++ src/window/put.c
@@ -4,22 +4,40 @@
 int tm_win_putch(Tm_window* win, char ch, uint32_t attrib) {
 	int ret = 0;
 
-	if(ch == '\n') {
-		if((ret = tm_win_cursor(win, 0, win->cursor_y + 1)) == TM_ERROR) {
-			return ret;
+	int position = win->cursor_y * win->columns + win->cursor_x;
+
+	if(win->flags & TM_FLAG_SCROLL) {
+		if(win->cursor_y > win->rows - 1 + win->buffer_position_y) {
+			tm_win_scroll(win, win->cursor_y - (win->rows - 1 + win->buffer_position_y), TM_SCROLL_DOWN);
 		}
+
+		else if(win->cursor_y < win->buffer_position_y) {		
+			tm_win_scroll(win, win->buffer_position_y - win->cursor_y, TM_SCROLL_UP);
+		}
+	}
+
+	else if(win->cursor_y > win->buffer_rows - 1 || win->cursor_y < 0) {
+		tm_error_number = TM_INVALID_CURSOR;
+		return TM_ERROR;
+	}
+
+
+	if(ch == '\n') {
+		win->cursor_y++;
+		win->cursor_x = 0;
 	}
 
 	else {
-		if((ret = tm_win_cursor(win, win->cursor_x + 1, win->cursor_y)) == TM_ERROR) {
-			win->buffer[win->cursor_y * win->buffer_columns + win->cursor_x].attrib = attrib;
-			win->buffer[win->cursor_y * win->buffer_columns + win->cursor_x].disp = ch;
-			return TM_ERROR;
-		}
+		win->buffer[win->cursor_y * win->buffer_columns + win->cursor_x].attrib = attrib;
+		win->buffer[win->cursor_y * win->buffer_columns + win->cursor_x].disp = ch;
 
-		win->buffer[win->cursor_y * win->buffer_columns + win->cursor_x - 1].attrib = attrib;
-		win->buffer[win->cursor_y * win->buffer_columns + win->cursor_x - 1].disp = ch;
+		win->cursor_x++;
 	}
+
+	position = win->cursor_y * win->columns + win->cursor_x;
+
+	win->cursor_x = position % win->columns;
+	win->cursor_y = position / win->columns;
 
 	return 0;
 }
