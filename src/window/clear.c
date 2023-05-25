ec4edf1c85173b6133dd81d6f398f3cc1c934593
8086ware <kirillos.khan08@outlook.com>
2023-05-25 15:47:31 -0400

    These shouldnt fail, so remove check

--- src/window/clear.c
+++ src/window/clear.c
@@ -3,9 +3,7 @@
 int tm_win_clear(Tm_window* win) {
 	int ret = 0;
 
-	if((ret = tm_win_cursor(win, 0, 0)) == TM_ERROR) {
-		return ret;
-	}
+	tm_win_cursor(win, 0, 0);
 	
 	for(int y = 0; y < win->buffer_rows; y++) {
 		for(int x = 0; x < win->buffer_columns; x++) {
@@ -16,9 +14,7 @@ int tm_win_clear(Tm_window* win) {
 	tm_win_attrib(win, TM_ATTRIB_ALL, 0);
 	tm_win_attrib(win, win->background_tm_char.disp, win->background_tm_char.attrib);
 	
-	if((ret = tm_win_cursor(win, 0, 0)) == TM_ERROR) {
-		return ret;
-	}
+	tm_win_cursor(win, 0, 0);
 
 	for(int i = 0; i < win->children_amount; i++) {
 		tm_win_clear(win->children[i]);
