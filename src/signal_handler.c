ebe790a4aea2e2edb5970d2412ffdc906f33b908
8086ware <kirillos.khan08@outlook.com>
2023-05-26 23:45:22 -0400

    Resize screen structure aswell when terminal has been resized

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -1,6 +1,7 @@
 #include <signal.h>
 #include "termmanip.h"
 #include <stdlib.h>
+#include "screen.h"
 
 void signal_handle(int sig) {
 #ifdef _WIN32
@@ -10,6 +11,8 @@ void signal_handle(int sig) {
 		tm_get_scrsize(&scr_x, &scr_y);
 		tm_win_modify(default_win, 0, 0, scr_x, scr_y);
 		tm_win_clear(default_win);
+		screen_resize();
+		tm_screen_update();
 		return;
 	}
 #endif
