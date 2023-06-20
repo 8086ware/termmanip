244e7020a87b0d6ed7a868c8d94a7f01038952ac
8086ware <kirillos.khan08@outlook.com>
2023-06-19 20:40:56 -0400

    Dont go through screen to check differences and update, just write screen output

--- src/screen/update.c
+++ src/screen/update.c
@@ -5,22 +5,6 @@
 #include <stdio.h>
 
 void tm_screen_update() {
-	for(int i = 0; i != screen->columns * screen->rows; i++) {
-		char disp = screen->buffer[i].disp;
-		uint32_t attrib = screen->buffer[i].attrib;
-
-		char physical_disp = screen->physical_buffer[i].disp;
-		uint32_t physical_attrib = screen->physical_buffer[i].attrib;
-
-		if(disp != physical_disp || attrib != physical_attrib) {
-
-			// If the screen attribute doesn't match the attribute we want to display then output the new attribute
-
-			screen_output_write(i % screen->columns, i / screen->columns, disp, attrib);
-		}
-
-	}
-	
 	screen_append_output("\x1b[%d;%dH", screen->cursor_y + 1, screen->cursor_x + 1);
 
 #ifdef _WIN32
