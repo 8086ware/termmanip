b8bfd32b00eedafb62dae84b7d8db76adf8a8011
8086ware <kirillos.khan08@outlook.com>
2023-07-07 21:21:15 -0400

    Make tm_set_error function

--- src/error.c
+++ src/error.c
@@ -1,7 +1,12 @@
 #include "termmanip.h"
+#include "error.h"
 
 int tm_error_number = 0;
 
 int tm_error() {
 	return tm_error_number;
+}
+
+void tm_set_error(int value) {
+	tm_error_number = value;
 }
