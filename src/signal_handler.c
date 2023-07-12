4ec9fd8988f97bb28dfb4e149e89fed7961a7a01
8086ware <kirillos.khan08@outlook.com>
2023-07-12 18:34:40 -0400

    Only call screen_resize since it does everything else

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -7,12 +7,7 @@ void signal_handle(int sig) {
 #ifdef _WIN32
 #else
 	if(sig == SIGWINCH) {
-		int scr_x, scr_y;
-		tm_get_scrsize(&scr_x, &scr_y);
-		tm_win_modify(default_win, 0, 0, scr_x, scr_y);
-		tm_win_clear(default_win);
 		screen_resize();
-		tm_screen_update();
 		return;
 	}
 #endif
