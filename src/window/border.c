506e20629f56d7e1dfd76952d283d608b60f77ca
8086ware <kirillos.khan08@outlook.com>
2023-07-12 18:36:07 -0400

    Change the way the border is set around the window (Fixes infinite scrolling glitch when setting scrolling flag and bordering window)

--- src/window/border.c
+++ src/window/border.c
@@ -4,19 +4,26 @@ void tm_win_border(Tm_window* win) {
 	int temp_x = win->cursor_x;
 	int temp_y = win->cursor_y;
 
-	for(int y = 0; y < win->buffer_rows; y++) {
-		for(int x = 0; x < win->buffer_columns; x++) {
-			tm_win_cursor(win, x, y);
-			if(x == 0 || x == win->buffer_columns - 1) {
-				tm_win_putch(win, '\x78', TM_ATTRIB_ALTERNATE | win->attrib);
-			}
-
-			if(y == 0 || y == win->buffer_rows - 1) {
-				tm_win_putch(win, '\x71', TM_ATTRIB_ALTERNATE | win->attrib);
-			}
-		}
+	for(int i = 0; i < tm_win_get_buffer_columns(win); i++) {
+		tm_win_cursor(win, i, 0);
+		tm_win_putch(win, '\x71', win->attrib | TM_ATTRIB_ALTERNATE);
 	}
-	
+
+	for(int i = 0; i < tm_win_get_buffer_columns(win); i++) {
+		tm_win_cursor(win, i, tm_win_get_buffer_rows(win) - 1);
+		tm_win_putch(win, '\x71', win->attrib | TM_ATTRIB_ALTERNATE);
+	}
+
+	for(int i = 0; i < tm_win_get_buffer_rows(win); i++) {
+		tm_win_cursor(win, 0, i);
+		tm_win_putch(win, '\x78', win->attrib | TM_ATTRIB_ALTERNATE);
+	}
+
+	for(int i = 0; i < tm_win_get_buffer_rows(win); i++) {
+		tm_win_cursor(win, tm_win_get_buffer_columns(win) - 1, i);
+		tm_win_putch(win, '\x78', win->attrib | TM_ATTRIB_ALTERNATE);
+	}
+
 	tm_win_cursor(win, 0, 0);
 	tm_win_putch(win, '\x6c', TM_ATTRIB_ALTERNATE | win->attrib);
 	tm_win_cursor(win, win->buffer_columns - 1, 0);
