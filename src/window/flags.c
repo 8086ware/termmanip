4acfe57de42aedd81f5e3c0ee0d891c563788153
8086ware <kirillos.khan08@outlook.com>
2023-07-12 23:05:41 -0400

    Move tm_win_get_flags to window/window.c

--- src/window/flags.c
+++ src/window/flags.c
@@ -1,9 +1,5 @@
 #include "termmanip.h"
 
-uint16_t tm_win_get_flags(Tm_window* win) {
-	return win->flags;
-}
-
 void tm_win_flags(Tm_window* win, uint16_t flags, int state) {
 	if(state <= 0) {
 		win->flags &= ~flags;
