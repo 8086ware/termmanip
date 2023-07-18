b90adf923975bbd7582c05b3dc4239d46f91b911
8086ware <kirillos.khan08@outlook.com>
2023-07-17 21:09:29 -0400

    Return int tm_init and return error when failing

--- src/init.c
+++ src/init.c
@@ -13,17 +13,28 @@
 
 Tm_screen* screen = NULL;
 
-void tm_init() {
-	term_init();
-	screen_init();
+int tm_init() {
+	if(term_init() == TM_ERROR) {
+		return TM_ERROR;
+	}
+
+	if(screen_init() == TM_ERROR) {
+		return TM_ERROR;
+	}
 
 	int scr_columns = 0, scr_rows = 0;
 	tm_get_scrsize(&scr_columns, &scr_rows);
 
 	default_win = tm_window(0, 0, scr_columns, scr_rows);
 
+	if(default_win == NULL) {
+		return TM_ERROR;
+	}
+
 	char init[] = "\x1b[?1049h\x1b[2J\x1b[H\x1b[0m";
 
 	write(fileno(stdout), init, strlen(init));
+
+	return 0;
 }
 
