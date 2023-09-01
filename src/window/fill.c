db50aa98de77a4b6aef70194a523f85cad587a50
8086ware <kirillos.khan08@outlook.com>
2023-09-01 04:09:49 -0400

    Use functions instead of directly accessing win

--- src/window/fill.c
+++ src/window/fill.c
@@ -1,7 +1,7 @@
 #include "termmanip.h"
 
 void tm_win_fill(Tm_window* win, int from_x, int from_y, int to_x, int to_y, char ch, uint32_t attrib) {
-	int cur_x = win->cursor_x, cur_y = win->cursor_y;
+	int cur_x = tm_win_get_cursor_x(win), cur_y = tm_win_get_cursor_y(win);
 
 	for(int y = from_y; y < to_y; y++) {
 		for(int x = from_x; x < to_x; x++) {
