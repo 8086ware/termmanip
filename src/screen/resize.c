0790335f3b9454266f1bd6aa88ad91907eb3739b
8086ware <kirillos.khan08@outlook.com>
2023-09-09 09:59:05 -0400

    Dont make variables columns and rows, and set the screen columns and rows after done resizing

--- src/screen/resize.c
+++ src/screen/resize.c
@@ -8,23 +8,16 @@ int screen_resize() {
 	tm_get_scrsize(&scr_x, &scr_y);
 	tm_win_modify(default_win, 0, 0, scr_x, scr_y);
 	tm_win_clear(default_win);
-	
-	int columns, rows;
-	tm_get_scrsize(&columns, &rows);
 
-	screen->buffer = realloc(screen->buffer, sizeof(Tm_char) * columns * rows);
+	screen->buffer = realloc(screen->buffer, sizeof(Tm_char) * scr_x * scr_y);
 
 	if(screen->buffer == NULL) {
 		tm_set_error(TM_OUT_OF_MEM);
 		return TM_ERROR;
 	}
 
-	for(int i = 0; i < columns * rows; i++) {
-		screen->buffer[i].disp = ' ';
-		screen->buffer[i].attrib = 0;
-	}
+	screen->columns = scr_x;
+	screen->rows = scr_y;
 
-	tm_screen_update();
-	
 	return 0;
 }
