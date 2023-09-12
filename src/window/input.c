2150f70b674101912bca3edcbbaa0fab67c5ebc1
8086ware <kirillos.khan08@outlook.com>
2023-09-12 16:26:54 -0400

    Dont set TM_FLAG_INPUTBLOCK and TM_FLAG_RAW in tm_win_input_str

--- src/window/input.c
+++ src/window/input.c
@@ -55,7 +55,7 @@ void tm_win_input_str(Tm_window* win, char* str, int max_size) {
 
 	int og_flags = tm_win_get_flags(win);
 
-	tm_win_flags(win, TM_FLAG_INPUTBLOCK | TM_FLAG_RAW | TM_FLAG_CURSOR_VISIBLE, 1);
+	tm_win_flags(win, TM_FLAG_CURSOR_VISIBLE, 1);
 	tm_win_flags(win, TM_FLAG_ECHO, 0);
 
 	while(1) {
