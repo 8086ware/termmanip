a84ec7e94dab642729d802b7a92cbb707987ff58
8086ware <kirillos.khan08@outlook.com>
2023-09-19 16:27:35 -0400

    Dont flush input buffer every time

--- src/window/input.c
+++ src/window/input.c
@@ -20,7 +20,6 @@ int tm_win_input_ch(Tm_window* win) {
 	INPUT_RECORD buffer;
 	_Bool read = 0;
 	do {
-		FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
 		if(WaitForSingleObject(GetStdHandle(STD_INPUT_HANDLE), win->input_timeout) == WAIT_OBJECT_0) {
 			DWORD bytes_read;
 			ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &buffer, 1, &bytes_read);
