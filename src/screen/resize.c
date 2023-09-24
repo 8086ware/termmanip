5527e0aba92e4c094ad6f4a46f105aff7d72055b
8086ware <kirillos.khan08@outlook.com>
2023-09-24 19:52:41 -0400

    Clear the screen and set screen cursor and other members to 0

--- src/screen/resize.c
+++ src/screen/resize.c
@@ -2,6 +2,7 @@
 #include "termmanip.h"
 #include <stdlib.h>
 #include "error.h"
+#include <stdio.h>
 
 int screen_resize() {
 	int scr_x, scr_y;
@@ -15,11 +16,22 @@ int screen_resize() {
 		return TM_ERROR;
 	}
 
+	screen->cursor_x = 0;
+	screen->cursor_y = 0;
+	screen->last_updated_x = 0;
+	screen->last_updated_y = 0;
+	screen->flags = TM_FLAG_ECHO;
+
+	free(screen->output);
+	screen->output_len = 0;
+	screen->attrib = 0;
+
 	for(int i = 0; i < scr_x * scr_y; i++) {
 		screen->buffer[i].disp = ' ';
 		screen->buffer[i].attrib = 0;
 	}
 
+	write(fileno(stdout), "\x1b[2J\x1b[H", 7);
 	screen->columns = scr_x;
 	screen->rows = scr_y;
 #ifdef _WIN32
