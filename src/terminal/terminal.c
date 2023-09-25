7575f4434da15584b3877ab61b873601d0feddf3
8086ware <kirillos.khan08@outlook.com>
2023-09-25 01:22:21 -0400

    Rename screen_init to terminal_init and merge term_init and screen_init and term_exit and screen_free

--- src/terminal/terminal.c
+++ src/terminal/terminal.c
@@ -1,53 +1,139 @@
 #include "termmanip.h"
-#include "screen.h"
+#include "terminal.h"
 #include <stdlib.h>
 #include "error.h"
+#include <stdio.h>
+#include "signal_handler.h"
+#include <signal.h>
 
-int screen_init() {
+#ifdef _WIN32
+#include <windows.h>
+DWORD og_input_mode;
+DWORD og_output_mode;
+#else
+#include <termios.h>
+#include <sys/signalfd.h>
+struct termios og_term;
+#endif
+
+int terminal_init() {
 	int scr_columns, scr_rows;
 	tm_get_scrsize(&scr_columns, &scr_rows);
-	screen = malloc(sizeof(Tm_screen));
+	terminal = malloc(sizeof(Tm_terminal));
 
-	if(screen == NULL) {
+	if(terminal == NULL) {
 		tm_set_error(TM_OUT_OF_MEM);
 		return TM_ERROR;
 	}
 
-	screen->columns = scr_columns;
-	screen->rows = scr_rows;
+	terminal->columns = scr_columns;
+	terminal->rows = scr_rows;
 
-	screen->cursor_x = 0;
-	screen->cursor_y = 0;
+	terminal->cursor_x = 0;
+	terminal->cursor_y = 0;
 
-	screen->last_updated_x = 0;
-	screen->last_updated_y = 0;
+	terminal->last_updated_x = 0;
+	terminal->last_updated_y = 0;
 
-	screen->output = NULL;
-	screen->output_len = 0;
+	terminal->output = NULL;
+	terminal->output_len = 0;
 
-	screen->attrib = 0;
+	terminal->attrib = 0;
 
-	screen->flags = TM_FLAG_CURSOR_VISIBLE | TM_FLAG_ECHO;
-	
-	screen->buffer = malloc(sizeof(Tm_char) * screen->columns * screen->rows);
+	terminal->flags = TM_FLAG_CURSOR_VISIBLE | TM_FLAG_ECHO;
 
-	if(screen->buffer == NULL) {
+	terminal->buffer = malloc(sizeof(Tm_char) * terminal->columns * terminal->rows);
+
+	if(terminal->buffer == NULL) {
 		tm_set_error(TM_OUT_OF_MEM);
 		return TM_ERROR;
 	}	
 
-	for(int i = 0; i < screen->columns * screen->rows; i++) {
-		screen->buffer[i].disp = ' ';
-		screen->buffer[i].attrib = 0;
+	for(int i = 0; i < terminal->columns * terminal->rows; i++) {
+		terminal->buffer[i].disp = ' ';
+		terminal->buffer[i].attrib = 0;
 	}
-	
+#ifdef _WIN32
+	DWORD mode = 0;
+
+	if(GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &mode) == 0) {
+		tm_set_error(TM_ERROR_COULDNT_INIT_TERM);
+		return TM_ERROR;
+	}
+
+	og_output_mode = mode;
+
+	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+	if(SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), mode) == 0) {
+		tm_set_error(TM_ERROR_COULDNT_INIT_TERM);
+		return TM_ERROR;
+	}
+
+	if(GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), &mode) == 0) {
+		tm_set_error(TM_ERROR_COULDNT_INIT_TERM);
+		return TM_ERROR;
+	}
+
+	og_input_mode = mode;
+
+	mode &= ~ENABLE_ECHO_INPUT;
+	mode &= ~ENABLE_LINE_INPUT;
+
+	mode |= ENABLE_VIRTUAL_TERMINAL_INPUT;
+
+	if(SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), mode) == 0) {
+		tm_set_error(TM_ERROR_COULDNT_INIT_TERM);
+		return TM_ERROR;
+	}
+
+	FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+#else 
+	struct termios term;
+	tcgetattr(fileno(stdin), &term);
+	og_term = term;
+
+	term.c_lflag &= ~ECHO;
+	term.c_lflag &= ~ICANON;
+
+	tcsetattr(fileno(stdin), TCSANOW, &term);
+#endif
+	signal(SIGINT, signal_handle);
+#ifndef _WIN32
+	sigset_t mask;
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGWINCH);
+
+	sigprocmask(SIG_BLOCK, &mask, NULL);
+	terminal->signal_fd = signalfd(-1, &mask, 0);
+
+	if(terminal->signal_fd == -1) {
+		tm_set_error(TM_ERROR_COULDNT_INIT_TERM);
+		return TM_ERROR;
+	}
+#endif
+
 	return 0;
 }
 
-void screen_free() {
-	free(screen->buffer);
-	free(screen->output);
-	free(screen);
+void terminal_free() {
+#ifdef _WIN32
+	if(SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), og_output_mode) == 0) {
+		tm_set_error(TM_ERROR_COULDNT_INIT_TERM);
+		return TM_ERROR;
+	}
 
-	screen = NULL;
+	if(SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), og_input_mode) == 0) {
+		tm_set_error(TM_ERROR_COULDNT_INIT_TERM);
+		return TM_ERROR;
+	}
+#else
+	tcsetattr(fileno(stdin), TCSANOW, &og_term);
+	tcsetattr(fileno(stdout), TCSANOW, &og_term);
+#endif
+	free(terminal->buffer);
+	free(terminal->output);
+	free(terminal);
+
+	terminal = NULL;
 }
