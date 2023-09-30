72601861e0b452afa173f22129552b27c93606a4
8086ware <kirillos.khan08@outlook.com>
2023-09-30 16:03:42 -0400

    When resizing, copy the original screen into the new screen

--- src/terminal/resize.c
+++ src/terminal/resize.c
@@ -1,27 +1,47 @@
 #include "terminal.h"
 #include "termmanip.h"
 #include <stdlib.h>
-#include "error.h"
+#include "error.h"	
 #include <stdio.h>
+#include <memory.h>
 
 int terminal_resize() {
 	int scr_x, scr_y;
 	tm_get_scrsize(&scr_x, &scr_y);
 	tm_win_modify(default_win, 0, 0, scr_x, scr_y);
 
+	Tm_char* temp = malloc(sizeof(Tm_char) * terminal->columns * terminal->rows);
+
+	memcpy(temp, terminal->buffer, sizeof(Tm_char) * terminal->columns * terminal->rows);
+
 	terminal->buffer = realloc(terminal->buffer, sizeof(Tm_char) * scr_x * scr_y);
 
-	if(terminal->buffer == NULL) {
-		tm_set_error(TM_OUT_OF_MEM);
-		return TM_ERROR;
-	}
+	int og_cols = terminal->columns, og_rows = terminal->rows;
 
 	terminal->columns = scr_x;
 	terminal->rows = scr_y;
-#ifdef _WIN32
-	COORD size = {scr_x, scr_y};
-	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), size);
-#endif
 
+	for(int i = 0; i < terminal->columns * terminal->rows; i++) {
+		Tm_char ch;
+		ch.attrib = 0;
+		ch.disp = ' ';
+
+		terminal_buffer_write(i % terminal->columns, i / terminal->columns, ch);
+	}
+
+	terminal->cursor_x = 0;
+	terminal->cursor_y = 0;		
+
+	for(int y = 0; y < og_rows; y++) {
+		for(int x = 0; x < og_cols; x++) {
+			if(x < terminal->columns && y < terminal->rows) {
+				terminal_buffer_write(x, y, temp[y * og_cols + x]);
+			}
+		}
+	}
+
+	free(temp);
+	
+	tm_terminal_update();
 	return 0;
 }
