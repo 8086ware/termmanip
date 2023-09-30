32fa737460e1415f0d16e80013ecbef15d90ecb0
8086ware <kirillos.khan08@outlook.com>
2023-09-30 16:48:05 -0400

    Check if position is out of terminal bounds

--- src/terminal/bufferwrite.c
+++ src/terminal/bufferwrite.c
@@ -4,6 +4,10 @@
 void terminal_buffer_write(int x, int y, Tm_char ch) {
 	int position = y * terminal->columns + x;
 
+	if(position > terminal->columns * terminal->rows || position < 0) {
+		return;
+	}
+
 	y = position / terminal->columns;
 	x = position % terminal->columns;
 
