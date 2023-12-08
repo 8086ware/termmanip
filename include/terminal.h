ebb8ec8840994877396cb827a9489decd82c1369
8086ware <kirillos.khan08@outlook.com>
2023-12-07 19:35:32 -0500

    Add terminal_make_output function

--- include/terminal.h
+++ include/terminal.h
@@ -12,6 +12,7 @@ void terminal_cursor(int x, int y);
 void terminal_attrib(uint32_t attrib);
 
 void terminal_write(int x, int y, char ch, uint32_t attrib);
+void terminal_make_output();
 int terminal_append_output(char* fmt, ...);
 
 int terminal_resize();
