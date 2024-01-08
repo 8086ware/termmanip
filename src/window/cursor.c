b13bfe1d7495d92dda5da93f5bacb25677d99283
8086ware <kirillos.khan08@outlook.com>
2024-01-07 22:40:03 -0500

    Detect if you want to scroll to the left or right as well

--- src/window/cursor.c
+++ src/window/cursor.c
@@ -4,25 +4,28 @@
 #include "return.h"
 
 int tm_win_cursor(Tm_window* win, int x, int y) {
-	int position = y * tm_win_get_buffer_columns(win) + x;
-
 	if(win->flags & TM_FLAG_SCROLL) {
-		if(y > tm_win_get_rows(win) - 1 + tm_win_get_buffer_pos_y(win)) {
-			tm_win_scroll(win, y - (tm_win_get_rows(win) - 1 + tm_win_get_buffer_pos_y(win)), TM_SCROLL_DOWN);
+		if(win->cursor_y > tm_win_get_rows(win) - 1 + tm_win_get_buffer_pos_y(win)) {
+			tm_win_scroll(win, win->cursor_y - (tm_win_get_rows(win) - 1 + tm_win_get_buffer_pos_y(win)), TM_SCROLL_DOWN);
 		}
 
-		else if(y < win->buffer_position_y) {		
-			tm_win_scroll(win, tm_win_get_buffer_pos_y(win)- y, TM_SCROLL_UP);
+		else if(win->cursor_y < win->buffer_position_y) {		
+			tm_win_scroll(win, tm_win_get_buffer_pos_y(win)- win->cursor_y, TM_SCROLL_UP);
+		}
+
+		if(win->cursor_x > tm_win_get_columns(win) - 1 + tm_win_get_buffer_pos_x(win)) {
+			tm_win_scroll(win, win->cursor_x - (tm_win_get_columns(win) - 1 + tm_win_get_buffer_pos_x(win)), TM_SCROLL_RIGHT);
+		}
+
+		else if(win->cursor_x < win->buffer_position_x) {		
+			tm_win_scroll(win, tm_win_get_buffer_pos_x(win)- win->cursor_x, TM_SCROLL_LEFT);
 		}
 	}
 
-	else if(position < 0 || position > tm_win_get_buffer_columns(win) * tm_win_get_buffer_rows(win)) {
+	else if(y < 0 || y > tm_win_get_buffer_rows(win) - 1 || x < 0 || x > tm_win_get_buffer_columns(win) - 1) {
 		tm_set_return(TM_INVALID_CURSOR);
 		return TM_ERROR;
 	}
-
-	x = position % tm_win_get_buffer_columns(win);
-	y = position / tm_win_get_buffer_columns(win);
 
 	win->cursor_x = x;
 	win->cursor_y = y;
