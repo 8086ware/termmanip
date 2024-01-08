0fdfb44882ebbd5a9eff66a3bb3321400781af0b
8086ware <kirillos.khan08@outlook.com>
2024-01-07 22:45:07 -0500

    Use the physical_buffer to draw the window on to the terminal and determine how many lines would be wrapped

--- src/window/update.c
+++ src/window/update.c
@@ -63,15 +63,43 @@ void tm_win_write_to_terminal(Tm_window* win) {
 		terminal->flags = tm_win_get_flags(win);
 	}
 
-	terminal_cursor((tm_win_get_cursor_x(win) - tm_win_get_buffer_pos_x(win)) + tm_win_get_pos_x(win) + parent_x, (tm_win_get_cursor_y(win) - tm_win_get_buffer_pos_y(win)) + tm_win_get_pos_y(win) + parent_y);
-
 	// Loop through window and put its buffer on the terminal buffer
+
+	for(int i = 0; i < win->columns * win->rows; i++) {
+		win->physical_buffer[i] = tm_win_get_background(win);
+	}
+
+	int wrapped_lines = 0;
+	for(int y = win->buffer_position_y; y < win->buffer_rows; y++) {
+		while(((y + wrapped_lines) - win->buffer_position_y) * win->columns < win->columns * win->rows) {
+			if(win->physical_buffer[((y + wrapped_lines) - win->buffer_position_y) * win->columns].disp != tm_win_get_background(win).disp || win->physical_buffer[((y + wrapped_lines) - win->buffer_position_y) * win->columns].attrib != tm_win_get_background(win).attrib) {
+				wrapped_lines++;
+			}
+
+			else {
+				break;
+			}
+		}
+
+		for(int x = win->buffer_position_x; x < win->buffer_columns; x++) {
+			if(((y + wrapped_lines) - win->buffer_position_y) * win->columns + (x - win->buffer_position_x) < win->columns * win->rows) {
+				Tm_char ch;
+				ch.disp = win->buffer[y * tm_win_get_buffer_columns(win) + x].disp;
+				ch.attrib = win->buffer[y * tm_win_get_buffer_columns(win) + x].attrib;
+				if(ch.disp != tm_win_get_background(win).disp || ch.attrib != tm_win_get_background(win).attrib) {
+					win->physical_buffer[(y + wrapped_lines - win->buffer_position_y) * win->columns + (x - win->buffer_position_x)] = ch;
+				}
+			}
+		}
+	}
+
+	terminal_cursor((tm_win_get_cursor_x(win) - tm_win_get_buffer_pos_x(win)) + tm_win_get_pos_x(win) + parent_x, (tm_win_get_cursor_y(win) + wrapped_lines - tm_win_get_buffer_pos_y(win)) + tm_win_get_pos_y(win) + parent_y);
 
 	for(int y = 0; y < win->rows; y++) {
 		for(int x = 0; x < win->columns; x++) {
 			Tm_char ch;
-			ch.disp = win->buffer[(tm_win_get_buffer_pos_y(win) + y) * tm_win_get_buffer_columns(win) + (tm_win_get_buffer_pos_x(win) + x)].disp;
-			ch.attrib = win->buffer[(tm_win_get_buffer_pos_y(win) + y) * tm_win_get_buffer_columns(win) + (tm_win_get_buffer_pos_x(win) + x)].attrib;
+			ch.disp = win->physical_buffer[y * tm_win_get_columns(win) + x].disp;
+			ch.attrib = win->physical_buffer[y * tm_win_get_columns(win) + x].attrib;
 
 			terminal_write((tm_win_get_pos_x(win) + parent_x + x), (tm_win_get_pos_y(win) + parent_y + y), ch.disp, ch.attrib);
 		}	
