dd03fa24ecbde78c358608b7370a105e910f5d69
8086ware <kirillos.khan08@outlook.com>
2024-01-07 22:36:39 -0500

    Add resize_buffer option to tm_win_modify which resizes the buffer and if its 0 then dont change the actual window buffer, only when the window is bigger then the buffer gets resized bigger.

--- src/window/modify.c
+++ src/window/modify.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "return.h"
 
-int tm_win_modify(Tm_window* win, int x, int y, int columns, int rows) {
+int tm_win_modify(Tm_window* win, int x, int y, int columns, int rows, _Bool resize_buffer) {
 	int scr_x, scr_y;
 
 	tm_get_scrsize(&scr_x, &scr_y);
@@ -37,6 +37,8 @@ int tm_win_modify(Tm_window* win, int x, int y, int columns, int rows) {
 	win->columns = columns;
 	win->rows = rows;
 
+	win->physical_buffer = realloc(win->physical_buffer, sizeof(Tm_char) * win->columns * win->rows);
+
 	if(win->buffer_columns < columns || win->buffer_rows < rows) {
 		int og_cols = win->buffer_columns, og_rows = win->buffer_rows;
 
@@ -44,7 +46,10 @@ int tm_win_modify(Tm_window* win, int x, int y, int columns, int rows) {
 		win->buffer_rows = rows;
 
 		Tm_char* temp = malloc(sizeof(Tm_char) * og_cols * og_rows);
-		memcpy(temp, win->buffer, sizeof(Tm_char) * og_cols * og_rows);
+
+		for(int i = 0; i < og_cols * og_rows; i++) {
+			temp[i] = win->buffer[i];
+		}
 
 		win->buffer = realloc(win->buffer, sizeof(Tm_char) * win->buffer_columns * win->buffer_rows);
 
@@ -59,9 +64,30 @@ int tm_win_modify(Tm_window* win, int x, int y, int columns, int rows) {
 		
 		for(int y = 0; y < og_rows; y++) {
 			for(int x = 0; x < og_cols; x++) {
-				win->buffer[y * win->buffer_columns + x] = temp[y * og_cols + x];
+				if(x < win->buffer_columns && y < win->buffer_rows) { 
+					win->buffer[y * win->buffer_columns + x] = temp[y * og_cols + x];
+				}
 			}
 		}
+
+		free(temp);
+	}
+
+	else if(resize_buffer) {
+		win->buffer_columns = columns;
+		win->buffer_rows = rows;
+
+		win->buffer = realloc(win->buffer, sizeof(Tm_char) * win->columns * win->rows);
+
+		if(win->buffer == NULL) {
+			tm_set_return(TM_OUT_OF_MEM);
+			return TM_ERROR;
+		}
+
+		tm_win_clear(win);
+
+		win->buffer_position_x = 0;
+		win->buffer_position_y = 0;
 	}
 
 	return 0;
