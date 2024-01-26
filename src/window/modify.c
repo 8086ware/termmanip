3952302c1ba5bad55506a59e4ef3409e33dad69e
8086ware <kirillos.khan08@outlook.com>
2024-01-26 00:00:01 -0500

    Scroll instead of reallocing

--- src/window/modify.c
+++ src/window/modify.c
@@ -56,38 +56,16 @@ int tm_win_modify(Tm_window* win, int x, int y, int columns, int rows, _Bool res
 		win->buffer_position_y = 0;
 	}
 
-	else if(win->buffer_columns < columns || win->buffer_rows < rows) {
-		int og_cols = win->buffer_columns, og_rows = win->buffer_rows;
-
-		win->buffer_columns = columns;
-		win->buffer_rows = rows;
-
-		Tm_char* temp = malloc(sizeof(Tm_char) * og_cols * og_rows);
-
-		for(int i = 0; i < og_cols * og_rows; i++) {
-			temp[i] = win->buffer[i];
+	else {
+		if(win->buffer_columns < columns) {
+			int og_buffer_pos_x = tm_win_get_buffer_pos_x(win), og_buffer_pos_y = tm_win_get_buffer_pos_y(win);
+			tm_win_scroll(win, columns - win->buffer_columns, TM_SCROLL_RIGHT);
 		}
 
-		win->buffer = realloc(win->buffer, sizeof(Tm_char) * win->buffer_columns * win->buffer_rows);
-
-		if(win->buffer == NULL) {
-			tm_set_return(TM_OUT_OF_MEM);
-			return TM_ERROR;
+		if(win->buffer_rows < rows) {
+			int og_buffer_pos_x = tm_win_get_buffer_pos_x(win), og_buffer_pos_y = tm_win_get_buffer_pos_y(win);
+			tm_win_scroll(win, rows - win->buffer_rows, TM_SCROLL_DOWN);
 		}
-
-		for(int i = 0; i < win->buffer_columns * win->buffer_rows; i++) { 
-			win->buffer[i] = tm_win_get_background(win);
-		}
-		
-		for(int y = 0; y < og_rows; y++) {
-			for(int x = 0; x < og_cols; x++) {
-				if(x < win->buffer_columns && y < win->buffer_rows) { 
-					win->buffer[y * win->buffer_columns + x] = temp[y * og_cols + x];
-				}
-			}
-		}
-
-		free(temp);
 	}
 
 	return 0;
