616d3a3ff23baaa664bac4b07b834b766f94f3b0
8086ware <kirillos.khan08@outlook.com>
2024-09-04 18:01:55 -0400

    Add type parameter to tm_win_parent and initalize window depending on child type

--- src/window/parent.c
+++ src/window/parent.c
@@ -2,9 +2,20 @@
 #include <stdlib.h>
 #include "return.h"
 
-int tm_win_parent(Tm_window* parent, Tm_window* child, int type) {
+int tm_win_parent(Tm_window* parent, Tm_window* child, enum Tm_child_type type) {
 	child->parent = parent;
 	child->child_type = type;
+
+	switch(type) {
+		case TM_CHILD_BUTTON:
+			tm_win_cursor(child, child->columns / 2 - child->name_length / 2, child->rows / 2);
+			tm_win_print(child, "%s", child->name);
+			break;
+		case TM_CHILD_TEXTBOX:
+			tm_win_print(child, "%s", child->name);
+			break;
+		break;
+	}
 	
 	parent->children = realloc(parent->children, (parent->children_amount + 1) * sizeof(Tm_window*));
 
@@ -15,6 +26,10 @@ int tm_win_parent(Tm_window* parent, Tm_window* child, int type) {
 
 	parent->children[parent->children_amount] = child;
 	
+	if(parent->children_amount == 0) {
+		parent->selected_child_window = child;
+	}
+
 	parent->children_amount++;
 
 	return 0;
