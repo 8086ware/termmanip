bff3ab36e0c19fef7630d5186b38c0c1745ae50a
8086ware <kirillos.khan08@outlook.com>
2024-10-13 18:53:02 -0400

    Remove tm_win_update and tm_win_write_to_terminal and add terminal_write_win_to_terminal to terminal.h

--- include/terminal.h
+++ include/terminal.h
@@ -9,5 +9,6 @@ void terminal_make_output(Tm_terminal* terminal);
 int terminal_append_output(Tm_terminal* terminal, char* fmt, ...);
 void update_terminal_flags(Tm_window* win);
 int terminal_resize(Tm_terminal* terminal);
+void terminal_write_win_to_terminal(Tm_window* win, int parent_x, int parent_y); // Writes the window to the terminal structure but doesn't display to the actual terminal
 
 #endif
