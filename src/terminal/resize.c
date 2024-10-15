c83cf14a1d514a7ae89de6af50c6dfb42f730da1
8086ware <kirillos.khan08@outlook.com>
2024-10-14 23:31:51 -0400

    Resize terminal by clearing the physical buffer and updating the terminal with the respect physical windows

--- src/terminal/resize.c
+++ src/terminal/resize.c
@@ -1,17 +1,10 @@
 #include "terminal.h"
 #include "termmanip.h"
 #include <stdlib.h>
-#include <memory.h>
 
 int terminal_resize(Tm_terminal* terminal) {
 	int scr_x, scr_y;
 	tm_get_termsize(&scr_x, &scr_y);
-
-	Tm_char* temp = malloc(sizeof(Tm_char) * terminal->columns * terminal->rows);
-
-	memcpy(temp, terminal->buffer, sizeof(Tm_char) * terminal->columns * terminal->rows);
-
-	int og_cols = terminal->columns, og_rows = terminal->rows;
 
 	terminal->columns = scr_x;
 	terminal->rows = scr_y;
@@ -19,27 +12,16 @@ int terminal_resize(Tm_terminal* terminal) {
 	terminal->buffer = realloc(terminal->buffer, sizeof(Tm_char) * terminal->columns * terminal->rows);
 	terminal->physical_buffer = realloc(terminal->physical_buffer, sizeof(Tm_char) * terminal->columns * terminal->rows);
 
-	for(int y = 0; y < terminal->rows; y++) {
-		for(int x = 0; x < terminal->columns; x++) {
-			terminal->buffer[y * terminal->columns + x].disp = ' ';
-			terminal->buffer[y * terminal->columns + x].attrib = 0;
-
-			terminal->physical_buffer[y * terminal->columns + x].disp = ' ';
-			terminal->physical_buffer[y * terminal->columns + x].attrib = 0;
-		}
-	}
-	
 	terminal_append_output(terminal, "\x1b[2J");
+	
+	Tm_char clear_ch;
 
-	for(int y = 0; y < og_rows; y++) {
-		for(int x = 0; x < og_cols; x++) {
-			terminal_write(terminal, x, y, temp[y * og_cols + x].disp, temp[y * og_cols + x].attrib);
-		}
-	}
+	clear_ch.disp = ' ';
+	clear_ch.attrib = 0;
+
+	memset(terminal->physical_buffer, &clear_ch, sizeof(Tm_char) * terminal->columns * terminal->rows);
 
 	tm_terminal_update(terminal);
-
-	free(temp);
 
 	return 0;
 }
