fe4c24540968536cca78082b42f861bec6be4a85
8086ware <kirillos.khan08@outlook.com>
2024-10-23 09:42:10 -0400

    Add new test files

---
+++ test/modify.c
@@ -0,0 +1,29 @@
+#include <termmanip.h>
+#include <stdlib.h>
+
+int main(void) {
+	Tm_terminal* terminal = tm_terminal();
+
+	int term_cols, term_rows;
+	tm_get_termsize(&term_cols, &term_rows);
+
+	Tm_window* win = tm_window(terminal, "test", 0, 0, term_cols, term_rows, NULL, TM_CHILD_NONE);
+
+	tm_win_flags(win, TM_FLAG_SCROLL, 1);
+
+	for(int i = 0; i < 1000; i++) {
+		tm_win_print(win, "%c", rand() % 48 + 48);
+		tm_win_update(win);
+	}
+
+	tm_win_flags(win, TM_FLAG_BORDER, 1);
+	for(int i = 0; i < 1000; i++) {
+		tm_win_modify(win, rand() % (term_cols - 1), rand() % (term_rows - 1), rand() % (term_cols - 1), rand() % (term_rows - 1), 0);
+		tm_win_cursor(win, rand() % tm_win_get_buffer_columns(win) - 1, rand() % tm_win_get_buffer_rows(win) - 1);
+		tm_win_print(win, "Hello World");
+		tm_win_update(win);
+	}
+
+	tm_win_free(win);
+	tm_terminal_free(terminal);
+}
---
+++ test/scroll.c
@@ -0,0 +1,35 @@
+#include <termmanip.h>
+#include <stdlib.h>
+
+int main(void) {
+	Tm_terminal* terminal = tm_terminal();
+
+	if(terminal == NULL) {
+		return 1;
+	}
+
+	int term_cols, term_rows;
+	tm_get_termsize(&term_cols, &term_rows);
+
+	Tm_window* win = tm_window(terminal, "test", 0, 0, term_cols, term_rows, NULL, TM_CHILD_NONE);
+
+	if(win == NULL) {
+		return 1;
+	}
+
+	tm_win_flags(win, TM_FLAG_SCROLL, 1);
+
+	for(int i = 0; i < 1000; i++) {
+		tm_win_print(win, "%c", rand() % 48 + 48);
+		tm_win_update(win);
+	}
+
+	for(int i = 0; i < 1000; i++) {
+		tm_win_cursor(win, 250 - rand() % 500, 250 - rand() % 500);
+		tm_win_print(win, "Test");
+		tm_win_update(win);
+	}
+
+	tm_win_free(win);
+	tm_terminal_free(terminal);
+}
