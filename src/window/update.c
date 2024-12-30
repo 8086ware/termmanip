732e116c5950785b4697964fde1cf77e51125dd3
8086ware <kirillos.khan08@outlook.com>
2024-12-30 02:00:29 -0500

    Update the child windows when updating window in the new update method

--- src/window/update.c
+++ src/window/update.c
@@ -3,11 +3,18 @@
 
 int tm_win_update(Tm_window* win) {
 	tm_win_mark_for_update(win);
+
 	return tm_terminal_update(win->terminal);
 }
 
 void tm_win_mark_for_update(Tm_window* win) {
 	win->update = 1;
-	win->terminal->last_updated_window = win;
+
 	terminal_win_order_topmost(win);
+
+	for (int i = 0; i < win->children_amount; i++) {
+		tm_win_mark_for_update(win->children[i]);
+	}
+
+	win->terminal->last_updated_window = win;
 }
